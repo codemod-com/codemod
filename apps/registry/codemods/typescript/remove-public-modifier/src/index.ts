3513f54c1fd542f9f646da79ccff2b44d3731b32
Benny Joo <sldisek783@gmail.com>
2024-01-31 17:26:39 +0000

    finalize

--- apps/registry/codemods/typescript/remove-public-modifier/src/index.ts
+++ apps/registry/codemods/typescript/remove-public-modifier/src/index.ts
@@ -1,24 +1,17 @@
 import type { API, FileInfo } from 'jscodeshift';
 
-/**
- * @param {import('jscodeshift').FileInfo} file
- * @param {import('jscodeshift').API} api
- */
 export default function transformer(file: FileInfo, api: API) {
 	const j = api.jscodeshift;
 	const root = j(file.source);
 
 	root.find(j.ClassDeclaration).forEach((path) => {
-		// Remove explicit 'public' modifiers from class members
 		path.node.body.body.forEach((member) => {
 			if (
-				member.type === 'MethodDefinition' ||
+				member.type === 'ClassMethod' ||
 				member.type === 'ClassProperty'
 			) {
-				// Remove modifiers
+				// @ts-expect-error "Property 'accessibility' does not exist on type 'ClassProperty'." is wrong
 				member.accessibility = undefined;
-				member.static = undefined;
-				member.readonly = undefined;
 			}
 		});
 	});
