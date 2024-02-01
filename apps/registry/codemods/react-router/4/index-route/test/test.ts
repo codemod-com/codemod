c80050c288138c00f9144837fa411f6a11635000
Dmytro Hryshyn <dev.dmytroh@gmail.com>
2024-02-01 13:44:38 +0200

    wip

---
+++ apps/registry/codemods/react-router/4/index-route/test/test.ts
@@ -0,0 +1,36 @@
+import assert from 'node:assert/strict';
+import { readFile } from 'node:fs/promises';
+import { join } from 'node:path';
+import { fileURLToPath } from 'node:url';
+import { buildApi } from '@codemod-com/utilities';
+import type { FileInfo } from 'jscodeshift';
+import { describe, it } from 'vitest';
+import transform from '../src/index.js';
+
+const __dirname = fileURLToPath(new URL('.', import.meta.url));
+
+describe('react-router v4 index-router', function () {
+	it('Should replace IndexRoute with Route with "exact" prop', async function () {
+		const input = await readFile(join(__dirname, 'input.js'), {
+			encoding: 'utf8',
+		});
+
+		const output = await readFile(join(__dirname, 'output.js'), {
+			encoding: 'utf8',
+		});
+
+		const fileInfo: FileInfo = {
+			path: 'index.js',
+			source: input,
+		};
+
+		const actualOutput = transform(fileInfo, buildApi('js'), {
+			quote: 'single',
+		});
+
+		assert.deepEqual(
+			actualOutput?.replace(/\W/gm, ''),
+			output.replace(/\W/gm, ''),
+		);
+	});
+});
