0f47f1c564c24ccfad2d66b6cc67eeea92d454e4
Benny Joo <sldisek783@gmail.com>
2024-02-01 16:21:53 +0000

    add test

---
+++ apps/registry/codemods/react-router/4/replace-location-query/test/test.ts
@@ -0,0 +1,41 @@
+import assert from 'node:assert/strict';
+import { buildApi } from '@codemod-com/utilities';
+import type { FileInfo } from 'jscodeshift';
+import { describe, it } from 'vitest';
+import transform from '../src/index.js';
+
+describe('react-router 4 replace-location-query', function () {
+	it('basic', function () {
+		const INPUT = `
+			const PostList = ({ location }) => (
+				<div>
+					<h1>List sorted by {location.query.sort}</h1>
+				</div>
+			);
+        `;
+
+		const OUTPUT = `
+			import { parse } from 'query-string';
+			const PostList = ({ location }) => {
+				const query = parse(location.search);
+				return (
+					<div>
+						<h1>List sorted by {query.sort}</h1>
+					</div>
+				);
+			};		
+        `;
+
+		const fileInfo: FileInfo = {
+			path: 'index.js',
+			source: INPUT,
+		};
+
+		const actualOutput = transform(fileInfo, buildApi('tsx'));
+
+		assert.deepEqual(
+			actualOutput?.replace(/\W/gm, ''),
+			OUTPUT.replace(/\W/gm, ''),
+		);
+	});
+});
