43c43d90fde1afc991d9e58a29fe9531047ee627
Benny Joo <sldisek783@gmail.com>
2024-02-01 12:34:47 +0000

    add tests for wrap-with-switch

--- packages/utilities/src/registry.ts
+++ packages/utilities/src/registry.ts
@@ -32,25 +32,25 @@ export const buildPathHashDigest = (path: string) =>
 
 export const getUnifiedEntry =
 	(fileSystem: IFs) =>
-		async (path: string): Promise<UnifiedEntry> => {
-			const stat = await fileSystem.promises.stat(path);
+	async (path: string): Promise<UnifiedEntry> => {
+		const stat = await fileSystem.promises.stat(path);
 
-			if (stat.isDirectory()) {
-				return {
-					kind: 'directory',
-					path,
-				};
-			}
+		if (stat.isDirectory()) {
+			return {
+				kind: 'directory',
+				path,
+			};
+		}
 
-			if (stat.isFile()) {
-				return {
-					kind: 'file',
-					path,
-				};
-			}
+		if (stat.isFile()) {
+			return {
+				kind: 'file',
+				path,
+			};
+		}
 
-			throw new Error(`The entry ${path} is neither a directory nor a file`);
-		};
+		throw new Error(`The entry ${path} is neither a directory nor a file`);
+	};
 
 export const buildGlobWrapper =
 	(fileSystem: IFs) => (globArguments: GlobArguments) => {
@@ -65,43 +65,43 @@ export const buildGlobWrapper =
 
 export const buildReadDirectory =
 	(fileSystem: IFs) =>
-		async (path: string): Promise<ReadonlyArray<UnifiedEntry>> => {
-			const entries = await fileSystem.promises.readdir(path, {
-				withFileTypes: true,
-			});
+	async (path: string): Promise<ReadonlyArray<UnifiedEntry>> => {
+		const entries = await fileSystem.promises.readdir(path, {
+			withFileTypes: true,
+		});
 
-			return entries.map((entry) => {
-				if (typeof entry === 'string' || !('isDirectory' in entry)) {
-					throw new Error('Entry can neither be a string or a Buffer');
-				}
+		return entries.map((entry) => {
+			if (typeof entry === 'string' || !('isDirectory' in entry)) {
+				throw new Error('Entry can neither be a string or a Buffer');
+			}
 
-				if (entry.isDirectory()) {
-					return {
-						kind: 'directory' as const,
-						path: join(path, entry.name.toString()),
-					};
-				}
+			if (entry.isDirectory()) {
+				return {
+					kind: 'directory' as const,
+					path: join(path, entry.name.toString()),
+				};
+			}
 
-				if (entry.isFile()) {
-					return {
-						kind: 'file' as const,
-						path: join(path, entry.name.toString()),
-					};
-				}
+			if (entry.isFile()) {
+				return {
+					kind: 'file' as const,
+					path: join(path, entry.name.toString()),
+				};
+			}
 
-				throw new Error('The entry is neither directory not file');
-			});
-		};
+			throw new Error('The entry is neither directory not file');
+		});
+	};
 
 export const buildReadFile =
 	(fileSystem: IFs) =>
-		async (path: string): Promise<string> => {
-			const data = await fileSystem.promises.readFile(path, {
-				encoding: 'utf8',
-			});
+	async (path: string): Promise<string> => {
+		const data = await fileSystem.promises.readFile(path, {
+			encoding: 'utf8',
+		});
 
-			return data.toString();
-		};
+		return data.toString();
+	};
 
 export const buildUnifiedFileSystem = (fileSystem: IFs) =>
 	new UnifiedFileSystem(
@@ -121,4 +121,4 @@ export const buildPathAPI = (currentWorkingDirectory: string): PathAPI => ({
 
 export const trimLicense = (testFixture: string): string => {
 	return testFixture.replace(/\/\*[\s\S]*?\*\//gm, '');
-}
+};
