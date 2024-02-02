b6e062d73b78903f13baaf04cbfede7892d095f2
Benny Joo <sldisek783@gmail.com>
2024-02-02 12:22:04 +0000

    [rename-imports] Add more test cases based on netlify codemod

--- apps/registry/codemods/react-router/4/rename-imports/test/test.ts
+++ apps/registry/codemods/react-router/4/rename-imports/test/test.ts
@@ -15,7 +15,67 @@ describe('react-router v4 rename-imports', function () {
 			source: input,
 		};
 
-		const actualOutput = transform(fileInfo, buildApi('js'), {
+		const actualOutput = transform(fileInfo, buildApi('ts'), {
+			quote: 'single',
+		});
+
+		assert.deepEqual(
+			actualOutput?.replace(/\W/gm, ''),
+			output.replace(/\W/gm, ''),
+		);
+	});
+
+	it('example 1 from netlify-react-ui', async function () {
+		const input = `import { browserHistory } from 'react-router';`;
+
+		const output = `import { browserHistory } from 'react-router-dom';`;
+
+		const fileInfo: FileInfo = {
+			path: 'index.js',
+			source: input,
+		};
+
+		const actualOutput = transform(fileInfo, buildApi('ts'), {
+			quote: 'single',
+		});
+
+		assert.deepEqual(
+			actualOutput?.replace(/\W/gm, ''),
+			output.replace(/\W/gm, ''),
+		);
+	});
+
+	it('example 2 from netlify-react-ui', async function () {
+		const input = `import type { WithRouterProps } from 'react-router';`;
+
+		const output = `import type { WithRouterProps } from 'react-router-dom';`;
+
+		const fileInfo: FileInfo = {
+			path: 'index.ts',
+			source: input,
+		};
+
+		const actualOutput = transform(fileInfo, buildApi('ts'), {
+			quote: 'single',
+		});
+
+		assert.deepEqual(
+			actualOutput?.replace(/\W/gm, ''),
+			output.replace(/\W/gm, ''),
+		);
+	});
+
+	it('example 3 from netlify-react-ui', async function () {
+		const input = `import { createMemoryHistory, Route, Router } from 'react-router';`;
+
+		const output = `import { createMemoryHistory, Route, Router } from 'react-router-dom';`;
+
+		const fileInfo: FileInfo = {
+			path: 'index.ts',
+			source: input,
+		};
+
+		const actualOutput = transform(fileInfo, buildApi('ts'), {
 			quote: 'single',
 		});
 
