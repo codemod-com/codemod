a788a5b33aa2a8b82b62fe196b749c2287ed229d
Benny Joo <sldisek783@gmail.com>
2024-02-02 16:24:15 +0000

    add more tests and improve the business logic to cover more cases (#26)

--- apps/registry/codemods/react-router/4/replace-location-query/test/test.ts
+++ apps/registry/codemods/react-router/4/replace-location-query/test/test.ts
@@ -140,4 +140,101 @@ describe('react-router 4 replace-location-query', function () {
 			OUTPUT.replace(/\W/gm, ''),
 		);
 	});
+
+	describe('examples from netlify-react-ui', function () {
+		it('example 1', function () {
+			const INPUT = `
+			function getOptions(props) {
+				return { ...props.location.query };
+			}
+        `;
+
+			const OUTPUT = `
+			import { parse } from 'query-string';
+
+			function getOptions(props) {
+				return { ...parse(props.location.search) };
+			}
+        `;
+
+			const fileInfo: FileInfo = {
+				path: 'index.js',
+				source: INPUT,
+			};
+
+			const actualOutput = transform(fileInfo, buildApi('tsx'));
+
+			actualOutput?.replace(/\W/gm, ''), OUTPUT.replace(/\W/gm, '');
+		});
+
+		it('example 2', function () {
+			const INPUT = `
+			function mapStateToProps(state: State, ownProps) {
+				const { site, plan } = ownProps.location.query;
+
+				return {
+					siteId: site,
+					highlightPlan: plan,
+				};
+			}
+			`;
+
+			const OUTPUT = `
+			import { parse } from 'query-string';
+			
+			function mapStateToProps(state: State, ownProps) {
+				const { site, plan } = parse(ownProps.location.search);
+
+				return {
+					siteId: site,
+					highlightPlan: plan,
+				};
+			}
+			`;
+
+			const fileInfo: FileInfo = {
+				path: 'index.js',
+				source: INPUT,
+			};
+
+			const actualOutput = transform(fileInfo, buildApi('tsx'));
+
+			assert.deepEqual(
+				actualOutput?.replace(/\W/gm, ''),
+				OUTPUT.replace(/\W/gm, ''),
+			);
+		});
+
+		it('example 3', function () {
+			const INPUT = `
+			const SiteAuditLog = (props) => {
+				const { page } = props.location.query;
+			  
+				return <ConnectedSiteAuditLog page={page} {...props} />;
+			};
+			`;
+
+			const OUTPUT = `
+			import { parse } from 'query-string';
+
+			const SiteAuditLog = (props) => {
+				const { page } = parse(props.location.search);
+			  
+				return <ConnectedSiteAuditLog page={page} {...props} />;
+			};
+			`;
+
+			const fileInfo: FileInfo = {
+				path: 'index.js',
+				source: INPUT,
+			};
+
+			const actualOutput = transform(fileInfo, buildApi('tsx'));
+
+			assert.deepEqual(
+				actualOutput?.replace(/\W/gm, ''),
+				OUTPUT.replace(/\W/gm, ''),
+			);
+		});
+	});
 });
