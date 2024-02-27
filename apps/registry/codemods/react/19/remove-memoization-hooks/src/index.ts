ccf0d47027289178d0f298ae662844e49e728014
Benny Joo <sldisek783@gmail.com>
2024-02-27 13:21:03 +0000

    cdmd-2575 [react codemod] remove-memoization-hooks (#91)

---
+++ apps/registry/codemods/react/19/remove-memoization-hooks/src/index.ts
@@ -0,0 +1,50 @@
+import type { API, FileInfo } from "jscodeshift";
+
+export default function transform(
+	file: FileInfo,
+	api: API,
+): string | undefined {
+	const j = api.jscodeshift;
+	const root = j(file.source);
+
+	const hooksToRemove = ["useMemo", "useCallback", "memo"];
+
+	root.find(j.ImportDeclaration).forEach((path) => {
+		if (path.node.specifiers?.length === 0) {
+			return;
+		}
+
+		const specifiers =
+			path.node.specifiers?.filter((specifier) => {
+				if (specifier.type === "ImportSpecifier") {
+					return !hooksToRemove.includes(specifier.imported.name);
+				}
+				return specifier;
+			}) ?? [];
+
+		if (specifiers.length === 0) {
+			j(path).remove();
+		} else {
+			path.node.specifiers = specifiers;
+		}
+	});
+
+	hooksToRemove.forEach((hook) => {
+		root
+			.find(j.MemberExpression, {
+				object: { name: "React" },
+				property: { name: hook },
+			})
+			.forEach((path) => {
+				j(path).remove();
+			});
+	});
+
+	hooksToRemove.forEach((hook) => {
+		root.find(j.Identifier, { name: hook }).forEach((path) => {
+			j(path).remove();
+		});
+	});
+
+	return root.toSource();
+}
