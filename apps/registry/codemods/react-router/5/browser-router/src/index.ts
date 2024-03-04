011ce48a586d1f9b8ea48835001135ef17d82c90
Benny Joo <sldisek783@gmail.com>
2024-03-04 14:52:57 +0000

    BrowserRouter codemod

---
+++ apps/registry/codemods/react-router/5/browser-router/src/index.ts
@@ -0,0 +1,48 @@
+import type { API, FileInfo } from "jscodeshift";
+
+function transform(file: FileInfo, api: API): string | undefined {
+	const j = api.jscodeshift;
+
+	const root = j(file.source);
+
+	root
+		.find(j.JSXElement, {
+			openingElement: { name: { name: "Router" } },
+		})
+		.forEach((path) => {
+			const attrs = path.value.openingElement.attributes;
+
+			const hasHistoryAttr =
+				attrs?.filter((a) => ("name" in a ? a.name.name === "history" : false))
+					.length ?? false;
+
+			if (attrs && hasHistoryAttr) {
+				if ("name" in path.value.openingElement.name) {
+					path.value.openingElement.name.name = "BrowserRouter";
+				}
+
+				if (
+					path.value.closingElement &&
+					"name" in path.value.closingElement.name
+				) {
+					path.value.closingElement.name.name = "BrowserRouter";
+				}
+
+				path.value.openingElement.attributes = attrs.filter((a) =>
+					"name" in a ? a.name.name !== "history" : false,
+				);
+			}
+
+			const computedImport = j.importDeclaration(
+				[j.importSpecifier(j.identifier("BrowserRouter"))],
+				j.literal("react-router-dom"),
+			);
+
+			const body = root.get().value.program.body;
+			body.unshift(computedImport);
+		});
+
+	return root.toSource();
+}
+
+export default transform;
---
+++ apps/registry/codemods/react-router/5/browser-router/test/test.ts
@@ -0,0 +1,39 @@
+import assert from "node:assert/strict";
+import { buildApi, trimLicense } from "@codemod-com/utilities";
+import type { FileInfo } from "jscodeshift";
+import { describe, it } from "vitest";
+import transform from "../src/index.js";
+
+describe("react-router v5 browser-router", () => {
+	it("should replace Router component with BrowserRouter, add BrowserRouter import", async () => {
+		const input = `
+		import { Router, browserHistory } from 'react-router';
+		const MyApp = () => (
+		<Router history={browserHistory}>
+			<Route path="/home" component={Home} />
+		</Router>
+		);
+		`;
+
+		const output = `
+		import { BrowserRouter } from 'react-router-dom';
+		import { Router, browserHistory } from 'react-router';
+		const MyApp = () => (
+		<BrowserRouter>
+			<Route path="/home" component={Home} />
+		</BrowserRouter>
+		);
+		`;
+		const fileInfo: FileInfo = {
+			path: "index.js",
+			source: trimLicense(input),
+		};
+
+		const actualOutput = transform(fileInfo, buildApi("js"));
+
+		assert.deepEqual(
+			actualOutput?.replace(/\W/gm, ""),
+			trimLicense(output).replace(/\W/gm, ""),
+		);
+	});
+});
