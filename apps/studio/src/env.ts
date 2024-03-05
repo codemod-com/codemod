58c5f6e1608d54a98ff1f1c559ee66cf18bd61d0
Benny Joo <sldisek783@gmail.com>
2024-03-05 16:45:55 +0000

    Fix codemod login

--- apps/studio/src/env.ts
+++ apps/studio/src/env.ts
@@ -16,6 +16,7 @@ export const env = createEnv({
 	client: {
 		NEXT_PUBLIC_API_URL: z.string(),
 		NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY: z.string(),
+		NEXT_PUBLIC_X_CODEMODCOM_ACCESS_TOKEN: z.string(),
 	},
 
 	/**
@@ -26,6 +27,8 @@ export const env = createEnv({
 		NODE_ENV: process.env.NODE_ENV,
 		NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL,
 		NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY:
+			process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY,
+		NEXT_PUBLIC_X_CODEMODCOM_ACCESS_TOKEN:
 			process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY,
 	},
 });
