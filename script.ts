35244c78d39bfa8290540afdcc11f37e2d1d813c
Sasha Mysak <reactionnnnn@gmail.com>
2024-03-13 16:22:59 +0100

    script

---
+++ script.ts
@@ -0,0 +1,32 @@
+import { exec } from "child_process";
+import { join } from "path";
+import { promisify } from "util";
+import { readdir } from "fs/promises";
+
+const execPromise = promisify(exec);
+
+// Function to recursively walk through directory tree
+const walkDirectory = async (dir, accumulator) => {
+	// Read directory contents
+	const entries = await readdir(dir, { withFileTypes: true });
+	for (const entry of entries) {
+		const fullPath = join(dir, entry.name);
+		if (entry.isDirectory()) {
+			// Recursively walk into directories
+			await walkDirectory(fullPath, accumulator);
+		} else if (entry.name === ".codemodrc.json") {
+			// If the entry is a .codemodrc.json file, accumulate the directory path
+			accumulator.push(dir);
+		}
+	}
+};
+
+(async () => {
+	const accumulator = [];
+	await walkDirectory("./packages/codemods", accumulator);
+
+	for (const dir of accumulator) {
+		// Run publish on the directory
+		await execPromise(`./apps/cli/dist/index.cjs --source ${dir}`);
+	}
+})();
