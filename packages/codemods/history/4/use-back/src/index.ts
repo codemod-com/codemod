4da341db48f3082d7c552250c33db97249749bc4
Benny Joo <sldisek783@gmail.com>
2024-03-13 12:00:27 +0000

    codemod logic

---
+++ packages/codemods/history/4/use-back/src/index.ts
@@ -0,0 +1,28 @@
+import type { API, FileInfo } from "jscodeshift";
+
+function transform(file: FileInfo, api: API): string | undefined {
+	const j = api.jscodeshift;
+
+	const root = j(file.source);
+
+	root
+		.find(j.MemberExpression, {
+			object: { name: "history" },
+			property: { name: "goBack" },
+		})
+		.forEach((path) => {
+			const identifierPath = j(path)
+				.find(j.Identifier, { name: "goBack" })
+				.paths()
+				.at(0);
+			if (!identifierPath) {
+				return;
+			}
+
+			identifierPath.replace(j.identifier.from({ name: "back" }));
+		});
+
+	return root.toSource();
+}
+
+export default transform;
