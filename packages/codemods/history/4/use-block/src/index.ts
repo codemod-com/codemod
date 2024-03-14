3ac39e62f4a18b0f47643b9d5e323dbf17bee919
Benny Joo <sldisek783@gmail.com>
2024-03-14 14:08:18 +0000

    finalize

--- packages/codemods/history/4/use-block/src/index.ts
+++ packages/codemods/history/4/use-block/src/index.ts
@@ -18,11 +18,63 @@ function transform(file: FileInfo, api: API): string | undefined {
 				.find(j.Identifier, { name: "listenBefore" })
 				.paths()
 				.at(0);
+
 			if (!identifierPath) {
 				return;
 			}
 
+			// Rename `listenBefore` to `block`
 			identifierPath.replace(j.identifier.from({ name: "block" }));
+
+			const arg = path.value.arguments[0];
+			if (!arg || !j.ArrowFunctionExpression.check(arg)) {
+				return;
+			}
+
+			const [locationNode, callbackNode] = arg.params;
+			const properties = [];
+
+			if (locationNode) {
+				properties.push(
+					j.objectProperty.from({
+						key: j.identifier("location"),
+						value: j.identifier("location"),
+						shorthand: true,
+					}),
+				);
+			}
+
+			if (callbackNode) {
+				properties.push(
+					j.objectProperty.from({
+						key: j.identifier("action"),
+						value: j.identifier("action"),
+						shorthand: true,
+					}),
+				);
+			}
+
+			if (properties.length === 0) {
+				return;
+			}
+
+			const objectPattern = j.objectPattern.from({
+				properties,
+			});
+
+			arg.params = [objectPattern];
+
+			if (j.Identifier.check(callbackNode)) {
+				const callbackName = callbackNode.name;
+				j(path)
+					.find(j.Identifier, {
+						type: "Identifier",
+						name: callbackName,
+					})
+					.forEach((path) => {
+						path.replace(j.identifier.from({ name: "action" }));
+					});
+			}
 		});
 
 	return root.toSource();
--- packages/codemods/history/4/use-block/test/test.ts
+++ packages/codemods/history/4/use-block/test/test.ts
@@ -1,18 +1,18 @@
 import assert from "node:assert/strict";
-import { buildApi, trimLicense } from "@codemod-com/utilities";
+import { buildApi } from "@codemod-com/utilities";
 import type { FileInfo } from "jscodeshift";
 import { describe, it } from "vitest";
 import transform from "../src/index.js";
 
 describe("history v4 use-block", () => {
-	it("should replace history.listenBefore() with history.block()", async () => {
+	it("should replace history.listenBefore() with history.block() when location is used", async () => {
 		const input = `
 		import createHistory from 'history/createBrowserHistory';
 
 		const history = createHistory();
 		
-		history.listenBefore((location, action) => {
-			console.log('test');
+		history.listenBefore(location => {
+			console.log(location);
 		});
 		`;
 
@@ -21,21 +21,57 @@ describe("history v4 use-block", () => {
 
 		const history = createHistory();
 		
-		history.block((location, action) => {
-			console.log('test');
+		history.block(({ location }) => {
+			console.log(location);
 		});
 		`;
 
 		const fileInfo: FileInfo = {
 			path: "index.js",
-			source: trimLicense(input),
+			source: input,
 		};
 
 		const actualOutput = transform(fileInfo, buildApi("js"));
 
 		assert.deepEqual(
-			actualOutput?.replace(/\W/gm, ""),
-			trimLicense(output).replace(/\W/gm, ""),
+			actualOutput?.replace(/[\s_;()]/gm, ""),
+			output.replace(/[\s_;()]/gm, ""),
+		);
+	});
+
+	it("should replace history.listenBefore() with history.block() when both location and action are used", async () => {
+		const input = `
+		import createHistory from 'history/createBrowserHistory';
+
+		const history = createHistory();
+		
+		history.listenBefore((location, callback) => {
+			console.log(location);
+			callback();
+		});
+		`;
+
+		const output = `
+		import createHistory from 'history/createBrowserHistory';
+
+		const history = createHistory();
+		
+		history.block(({ location, action }) => {
+			console.log(location);
+			action();
+		});
+		`;
+
+		const fileInfo: FileInfo = {
+			path: "index.js",
+			source: input,
+		};
+
+		const actualOutput = transform(fileInfo, buildApi("js"));
+
+		assert.deepEqual(
+			actualOutput?.replace(/[\s_;()]/gm, ""),
+			output.replace(/[\s_;()]/gm, ""),
 		);
 	});
 });
