481be90e33a8f234ff894fb7a4701bdb84ac26fc
Benny Joo <sldisek783@gmail.com>
2024-03-14 10:28:59 +0000

    add more test case and improve existing one

--- packages/codemods/history/4/replace-state-object/test/test.ts
+++ packages/codemods/history/4/replace-state-object/test/test.ts
@@ -5,13 +5,13 @@ import { describe, it } from "vitest";
 import transform from "../src/index.js";
 
 describe("history v4 use-back", () => {
-	it("should replace history.push(path, { state }) with history.push(path, state)", async () => {
+	it("should extract `state` object into the second argument", async () => {
 		const input = `
 		import createHistory from 'history/createBrowserHistory';
 
 		const history = createHistory();
 		
-		history.push('/new-path', { state: { key: 'value' } });
+		history.push({ pathname: '/new-path', search: 'search', hash: 'hash', state: { key: 'value' } });
 		`;
 
 		const output = `
@@ -19,7 +19,7 @@ describe("history v4 use-back", () => {
 
 		const history = createHistory();
 		
-		history.push('/new-path', { key: 'value' });
+		history.push({ pathname: '/new-path', search: 'search', hash: 'hash' }, { key: 'value' });
 		`;
 
 		const fileInfo: FileInfo = {
@@ -29,6 +29,39 @@ describe("history v4 use-back", () => {
 
 		const actualOutput = transform(fileInfo, buildApi("js"));
 
-		assert.deepEqual(actualOutput, output);
+		assert.deepEqual(
+			actualOutput?.replace(/\W/gm, ""),
+			output.replace(/\W/gm, ""),
+		);
+	});
+
+	it("should do nothing if the argument is simply a string.", async () => {
+		const input = `
+		import createHistory from 'history/createBrowserHistory';
+
+		const history = createHistory();
+		
+		history.push('/new-path');
+		`;
+
+		const output = `
+		import createHistory from 'history/createBrowserHistory';
+
+		const history = createHistory();
+		
+		history.push('/new-path');
+		`;
+
+		const fileInfo: FileInfo = {
+			path: "index.js",
+			source: input,
+		};
+
+		const actualOutput = transform(fileInfo, buildApi("js"));
+
+		assert.deepEqual(
+			actualOutput?.replace(/\W/gm, ""),
+			output.replace(/\W/gm, ""),
+		);
 	});
 });
