187b3e8bfef7faf8347627769b1578142f841aba
Mikhail Demidoff <demidoff.mikhail@gmail.com>
2024-03-21 19:19:52 +0200

    created a new migration & bumped version to 0.0.45 (#213)
    Co-authored-by: mirai2k <mikhail.demidoff@gmail.com>

--- apps/backend/prisma/migrations/20240321170952_add_description_usecasecategory_tags_engine_applicability_to_codemods_table/migration.sql
+++ apps/backend/prisma/migrations/20240321170952_add_description_usecasecategory_tags_engine_applicability_to_codemods_table/migration.sql
@@ -1,12 +1,6 @@
-/*
-  Warnings:
-
-  - Added the required column `engine` to the `Codemod` table without a default value. This is not possible if the table is not empty.
-
-*/
 -- AlterTable
 ALTER TABLE "Codemod" ADD COLUMN     "applicability" JSONB,
-ADD COLUMN     "engine" VARCHAR(255) NOT NULL,
+ADD COLUMN     "engine" VARCHAR(255),
 ADD COLUMN     "shortDescription" TEXT,
 ADD COLUMN     "tags" TEXT[] DEFAULT ARRAY[]::TEXT[],
 ADD COLUMN     "useCaseCategory" VARCHAR(255);
