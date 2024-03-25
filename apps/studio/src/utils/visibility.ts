1a85d319c0c66f51f8b35a4f19acbdb0c179ede3
Greg <niebadz@gmail.com>
2024-03-25 15:10:02 +0100

    type fix

--- apps/studio/src/utils/visibility.ts
+++ apps/studio/src/utils/visibility.ts
@@ -1,4 +1,5 @@
 import { VisibilityOptions } from "~/types/options";
+import { isNil } from "~/utils/isNil";
 
 export const isVisible = (
 	visibility:
@@ -7,8 +8,8 @@ export const isVisible = (
 		  }
 		| { isVisible: boolean },
 ) =>
-	"visibilityOptions" in visibility
-		? visibility?.visibilityOptions?.isVisible
+	"visibilityOptions" in visibility && !isNil(visibility?.visibilityOptions)
+		? visibility.visibilityOptions.isVisible
 		: "isVisible" in visibility
 		  ? visibility.isVisible
 		  : true;
