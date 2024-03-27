dce9e7d3b17cb924b88c54b9b5f68124ffaf61a4
Grzegorz <niebadz@gmail.com>
2024-03-27 16:39:12 +0100

    [studio] feat: Bottom pane functional improvements (#224)

--- apps/studio/src/utils/visibility.ts
+++ apps/studio/src/utils/visibility.ts
@@ -1,10 +1,18 @@
 import { VisibilityOptions } from "~/types/options";
+import { isNil } from "~/utils/isNil";
 
-export const isVisible = ({
-	visibilityOptions,
-}: {
-	visibilityOptions?: VisibilityOptions;
-}) => visibilityOptions?.isVisible ?? true;
+export const isVisible = (
+	visibility:
+		| {
+				visibilityOptions?: VisibilityOptions;
+		  }
+		| { isVisible: boolean },
+) =>
+	"visibilityOptions" in visibility && !isNil(visibility?.visibilityOptions)
+		? visibility.visibilityOptions.isVisible
+		: "isVisible" in visibility
+		  ? visibility.isVisible
+		  : true;
 export const alwaysVisible: VisibilityOptions = {
 	isVisible: true,
 	toggleVisibility: () => true,
