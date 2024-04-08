54d962647b5ee634bd909690e8047ab35caa85c7
Greg <niebadz@gmail.com>
2024-04-08 20:25:39 +0200

    state added

--- apps/studio/src/pageComponents/main/Header.tsx
+++ apps/studio/src/pageComponents/main/Header.tsx
@@ -1,11 +1,27 @@
 import { KnownEngines } from "@codemod-com/utilities";
+import { useAuth } from "@clerk/nextjs";
 import { Backspace as BackspaceIcon } from "@phosphor-icons/react";
 import { Link as LinkIcon } from "@phosphor-icons/react";
+import { Check as CheckIcon } from "@phosphor-icons/react";
+import { useRouter } from "next/navigation";
+import { pipe } from "ramda";
+import { useEffect, useRef, useState } from "react";
 import AuthButtons from "~/auth/AuthButtons";
 import { Button } from "~/components/ui/button";
+import {
+	Select,
+	SelectContent,
+	SelectItem,
+	SelectTrigger,
+	SelectValue,
+} from "~/components/ui/select";
+import { authUrl } from "~/config";
+import { useModal } from "~/hooks/useModal";
 import { CodemodLogo } from "~/icons/CodemodLogo";
+import { cn } from "~/lib/utils";
 import { useModStore } from "~/store/zustand/mod";
 import { useSnippetStore } from "~/store/zustand/snippets";
+import { useUserState } from "~/store/zustand/user";
 import { DownloadZip } from "./DownloadZip";
 import { useTheme } from "./themeContext";
 import { usePublicLinkSharing } from "./usePublicLinkSharing";
@@ -24,8 +40,76 @@ const Header = () => {
 	const { getShareLink, isCreating: isShareURLBeingCreated } =
 		usePublicLinkSharing();
 
+	const router = useRouter();
+	const { isSignedIn, getToken } = useAuth();
+	const [repositoriesToShow, setRepositoriesToShow] = useState<string[]>([]);
+	const {
+		showModal: showRepositoryModal,
+		hideModal,
+		isModalShown,
+	} = useModal();
+	const [selectedRepository, setSelectedRepository] = useState<string>();
+	const getRepositories = async () => {
+		const token = await getToken();
+		// call the API for repos
+		const repositories = ["repo 1", "repo 2"];
+		setRepositoriesToShow(repositories);
+	};
+	const {
+		pendingActionsWhenSigned,
+		retrievePendingAction,
+		addPendingActionsWhenSigned,
+	} = useUserState();
+	const showRepositoryModalFlow = pipe(getRepositories, showRepositoryModal);
+	const redirectToGHAuthFlow = pipe(
+		() => addPendingActionsWhenSigned("openRepoModal"),
+		() => router.push(authUrl),
+	);
+	const shouldOpenPendingRepoModal = retrievePendingAction("openRepoModal");
+	console.log({ pendingActionsWhenSigned }, { shouldOpenPendingRepoModal });
+	const selectRepository = (repository: string) => console.log(repository);
+
+	useEffect(() => {
+		if (shouldOpenPendingRepoModal && isSignedIn) showRepositoryModalFlow();
+	}, [shouldOpenPendingRepoModal]);
+
+	const dialog = useRef<HTMLDialogElement>(null);
+	useEffect(() => {
+		if (isModalShown) dialog.current?.showModal();
+	}, [isModalShown]);
+
+	const ensureSignIn = isSignedIn
+		? showRepositoryModalFlow
+		: redirectToGHAuthFlow;
 	return (
 		<>
+			<dialog ref={dialog} style={{ width: "500px", height: "500px" }}>
+				<Select onValueChange={selectRepository} value={selectedRepository}>
+					<SelectTrigger className="flex w-full select-none items-center font-semibold">
+						<span
+							className={cn("mr-[0.75rem] text-xs font-light text-slate-500", {
+								"text-slate-200": isDark,
+							})}
+						>
+							Select repository:
+						</span>
+						<SelectValue placeholder={selectedRepository} />
+					</SelectTrigger>
+					<SelectContent>
+						{repositoriesToShow.map((e) => (
+							<SelectItem
+								key={e}
+								value={e}
+								className={cn({
+									"font-semibold": selectedRepository === e,
+								})}
+							>
+								{e}
+							</SelectItem>
+						))}
+					</SelectContent>
+				</Select>
+			</dialog>
 			<div className="flex justify-between h-[50px] w-full flex-1 bg-white p-1">
 				<Button
 					variant="link"
@@ -41,6 +125,16 @@ const Header = () => {
 				<div />
 				<div className="flex gap-2">
 					{/* FIXME: remove text-white and specific classNames once buttons are moved to the new design */}
+					<Button
+						size="xs"
+						variant="outline"
+						className="flex gap-1"
+						hint={<p className="font-normal">Run Codemod</p>}
+						onClick={ensureSignIn}
+					>
+						<CheckIcon className="h-4 w-4" />
+						Run Codemod
+					</Button>
 					<Button
 						size="xs"
 						variant="outline"
