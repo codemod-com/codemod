4e7a9150c0ad9e9d989580d9fd4fac73b1053f42
Benny Joo <sldisek783@gmail.com>
2024-04-09 14:53:44 +0100

    [Studio] Add /getExecutionStatus endpoint handler

--- apps/studio/src/constants/apiEndpoints.ts
+++ apps/studio/src/constants/apiEndpoints.ts
@@ -1,5 +1,6 @@
 const SEND_MESSAGE = "sendMessage";
+const GET_EXECUTION_STATUS = "getExecutionStatus";
 const SEND_CHAT = "sendChat";
 const BUILD_ACCESS_TOKEN = "buildAccessToken";
 
-export { SEND_MESSAGE, SEND_CHAT, BUILD_ACCESS_TOKEN };
+export { SEND_MESSAGE, SEND_CHAT, BUILD_ACCESS_TOKEN, GET_EXECUTION_STATUS };
