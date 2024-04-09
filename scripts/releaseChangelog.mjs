944157aa788d617de829c0264e44e85781bcc915
Benny Joo <sldisek783@gmail.com>
2024-04-09 21:25:12 +0100

    improve

--- scripts/releaseChangelog.mjs
+++ scripts/releaseChangelog.mjs
@@ -1,14 +1,16 @@
 import fetch from "node-fetch";
 
+const CODEMOD_REPO_PULL_REQUESTS =
+	"https://api.github.com/repos/codemod-com/codemod/pulls";
+
 async function getPullRequestTitles() {
-	const url = "https://api.github.com/repos/codemod-com/codemod/pulls";
 	const params = new URLSearchParams({
 		state: "closed",
 		sort: "created",
 		direction: "desc",
-		base: "main", // Optional: filter pull requests that were merged into this branch
+		base: "main", // Optional: filter pull requests that were merged into `main` branch
 	});
-	const response = await fetch(`${url}?${params}`);
+	const response = await fetch(`${CODEMOD_REPO_PULL_REQUESTS}?${params}`);
 	// The GitHub REST API limits the number of items returned in a single response; e.g., 30 items per page
 	const pullRequests = await response.json();
 	const titles = pullRequests.map((pullRequest) => pullRequest.title);
