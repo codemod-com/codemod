d2c3ae7613e878e646e480d8bc40b6cd7ae2446b
DmytroHryshyn <dmytro@codemod.com>
2024-04-10 11:18:42 +0300

    [CODEMOD:FN]: replace-params (#206)

--- packages/codemods/react-router/4/replace-param-prop/test/test.ts
+++ packages/codemods/react-router/4/replace-param-prop/test/test.ts
@@ -7,19 +7,63 @@ import transform from "../src/index.js";
 describe("react-router v4 replace-param-prop", () => {
 	it('should replace "params" prop with "match.params" ', async () => {
 		const input = `
-		const PostEdit = ({ params }) => (
-			<div>
+		const PostEdit = ({ params }) => {
+			return <div>
 				<h1>Post {params.id}</h1>
 			</div>
-		)
+		}
 		`;
 
 		const output = `
-		const PostEdit = ({ match }) => (
-			<div>
-				<h1>Post {match.params.id}</h1>
+		const PostEdit = ({ match }) => {
+			const { params } = match;
+			return <div>
+				<h1>Post {params.id}</h1>
 			</div>
-		)
+		}
+		`;
+
+		const fileInfo: FileInfo = {
+			path: "index.js",
+			source: input,
+		};
+
+		const actualOutput = transform(fileInfo, buildApi("js"), {
+			quote: "single",
+		});
+
+		assert.deepEqual(
+			actualOutput?.replace(/\W/gm, ""),
+			output.replace(/\W/gm, ""),
+		);
+	});
+
+	it('should replace "params" prop with "match.params" in mapStateToProps', async () => {
+		const input = `
+		const PostEdit = () => {
+			return null;
+		}
+
+		const mapStateToProps = (state, { params }) => {
+
+			return {
+				a: params.a
+			}
+		}
+
+		`;
+
+		const output = `
+		const PostEdit = () => {
+			return null;
+		}
+
+		const mapStateToProps = (state, { match }) => {
+			const { params } = match;
+			return {
+				a: params.a
+			}
+		}
 		`;
 
 		const fileInfo: FileInfo = {
