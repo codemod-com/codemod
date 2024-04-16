6ce8a3485c04d42f4a74082c0c1e2e1ff450e0ac
Benny Joo <sldisek783@gmail.com>
2024-04-16 18:02:08 +0100

    add getCodemodList api handler

---
+++ apps/studio/src/api/getCodemodList.ts
@@ -0,0 +1,15 @@
+import type { CodemodListResponse } from "@codemod-com/utilities";
+
+import { GET_CODEMODS_LIST } from "~/constants";
+import apiClient from "./client";
+
+export const getCodemodList = async (): Promise<CodemodListResponse | null> => {
+	try {
+		const res = await apiClient.get<CodemodListResponse>(GET_CODEMODS_LIST);
+
+		return res.data;
+	} catch (e) {
+		console.error(e);
+		return null;
+	}
+};
--- apps/studio/src/constants/apiEndpoints.ts
+++ apps/studio/src/constants/apiEndpoints.ts
@@ -2,5 +2,12 @@ const SEND_MESSAGE = "sendMessage";
 const GET_EXECUTION_STATUS = "getExecutionStatus";
 const SEND_CHAT = "sendChat";
 const BUILD_ACCESS_TOKEN = "buildAccessToken";
+const GET_CODEMODS_LIST = "/codemods/list";
 
-export { SEND_MESSAGE, SEND_CHAT, BUILD_ACCESS_TOKEN, GET_EXECUTION_STATUS };
+export {
+	SEND_MESSAGE,
+	GET_CODEMODS_LIST,
+	SEND_CHAT,
+	BUILD_ACCESS_TOKEN,
+	GET_EXECUTION_STATUS,
+};
