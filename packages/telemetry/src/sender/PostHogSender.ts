d069e5931356190d4f8ae098e9ab5fb9af5a3e99
Dmytro Hryshyn <dmytro@codemod.com>
2024-04-18 12:41:12 +0300

    bump version, fix host

--- packages/telemetry/src/sender/PostHogSender.ts
+++ packages/telemetry/src/sender/PostHogSender.ts
@@ -14,7 +14,7 @@ export class PostHogSender<Event extends BaseEvent>
 	constructor(private readonly __options: TelemetrySenderOptions) {
 		this.__telemetryClient = new PostHog(
 			"phc_nGWKWP3t1fcNFqGi6UdstXjMf0fxx7SBeohHPSS6d2Y",
-			{ host: "https://eu.posthog.com" },
+			{ host: "https://app.posthog.com" },
 		);
 	}
 
