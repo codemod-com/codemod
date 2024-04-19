5df83ef3b6159746272e72d559858bd8742d1f7e
Mikhail Demidoff <demidoff.mikhail@gmail.com>
2024-04-19 17:51:14 +0300

    fix pnpm and deploy (#375)
    Co-authored-by: mirai2k <mikhail.demidoff@gmail.com>

--- apps/backend/src/services/codemodService.ts
+++ apps/backend/src/services/codemodService.ts
@@ -241,12 +241,12 @@ export class CodemodService {
 			throw new CodemodNotFoundError();
 		}
 
-		const frameworkTags = await this.prisma.tag.findMany({
-			where: { classification: "framework" },
-		});
-
 		const useCaseCategoryTags = await this.prisma.tag.findMany({
 			where: { classification: "useCaseCategory" },
+		});
+
+		const frameworkTags = await this.prisma.tag.findMany({
+			where: { classification: "framework" },
 		});
 
 		const useCaseCategory = useCaseCategoryTags.find((tag) =>
