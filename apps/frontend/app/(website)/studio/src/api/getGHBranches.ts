64fa4186853d16033f4330359f58bd4a900bf8ef
Benny Joo <sldisek783@gmail.com>
2024-04-19 06:39:16 +0100

    frontend work

---
+++ apps/frontend/app/(website)/studio/src/api/getGHBranches.ts
@@ -0,0 +1,36 @@
+import type { AxiosError } from "axios";
+import type { GithubRepository } from "be-types";
+import { GH_BRANCH_LIST } from "../constants";
+import { Either } from "../utils/Either";
+import apiClient from "./client";
+
+type GetGHBranchesResponse = Readonly<string[]>;
+
+type GetGHBranchesRequest = Readonly<{
+	repo: GithubRepository;
+	token: string;
+}>;
+
+const getGHBranches = async ({
+	repo,
+	token,
+}: GetGHBranchesRequest): Promise<Either<Error, GetGHBranchesResponse>> => {
+	try {
+		const res = await apiClient.post<GetGHBranchesResponse>(
+			GH_BRANCH_LIST,
+			repo,
+			{
+				headers: {
+					Authorization: `Bearer ${token}`,
+				},
+			},
+		);
+
+		return Either.right(res.data);
+	} catch (e) {
+		const err = e as AxiosError<{ message?: string }>;
+		return Either.left(new Error(err.response?.data.message ?? err.message));
+	}
+};
+
+export default getGHBranches;
