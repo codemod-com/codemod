85401b97bcd530cf57ae19c1f4f2b40d85e4ebdb
Benny Joo <sldisek783@gmail.com>
2024-04-23 14:54:27 +0100

    [Studio] Redirect users accessing codemod.studio to codemod.com/studio - CDMD-3135 (#355)

---
+++ apps/frontend/app/(website)/studio/src/constants/urls.ts
@@ -0,0 +1,2 @@
+export const OLD_STUDIO_HOSTNAME = "codemod.studio";
+export const CODEMOD_STUDIO_URL = "https://codemod.com/studio";
--- apps/frontend/middleware.ts
+++ apps/frontend/middleware.ts
@@ -1,10 +1,18 @@
 import { getRedirect } from "@/data/sanity/redirects";
-import type { NextRequest } from "next/server";
-import { NextResponse } from "next/server";
+import { type NextRequest, NextResponse } from "next/server";
+import {
+	CODEMOD_STUDIO_URL,
+	OLD_STUDIO_HOSTNAME,
+} from "./app/(website)/studio/src/constants/urls";
 
 // @TODO: Handle redirects from Sanity
 export async function middleware(request: NextRequest) {
+	if (request.nextUrl.hostname === OLD_STUDIO_HOSTNAME) {
+		return NextResponse.redirect(new URL(CODEMOD_STUDIO_URL));
+	}
+
 	const pathname = request.nextUrl.pathname;
+	// @TODO fix sanity in middleware error
 	const redirect = await getRedirect(pathname);
 
 	if (redirect) {
