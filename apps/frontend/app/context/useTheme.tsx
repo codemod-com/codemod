1148a0ce033c09efa7e8ef26c55a1a85692f9029
Benny Joo <sldisek783@gmail.com>
2024-04-24 19:07:10 +0100

    [studio] force-set theme to light mode until dark theme is stable - CDMD-3194

--- apps/frontend/app/context/useTheme.tsx
+++ apps/frontend/app/context/useTheme.tsx
@@ -17,48 +17,48 @@ const UseTheme = createContext<{
   isDark: null,
   toggleTheme: () => {},
 });
-const THEME_CONST = "_THEME_";
-const LIGHT = "light";
-const DARK = "dark";
+// const THEME_CONST = "_THEME_";
+// const LIGHT = "light";
+// const DARK = "dark";
 
 const ThemeProvider = ({ children }: { children: ReactNode }) => {
-  const [isDark, setDarkTheme] = useState<boolean | null>(null);
+  // const [isDark, setDarkTheme] = useState<boolean | null>(null);
 
-  useLayoutEffect(() => {
-    const storedTheme = localStorage.getItem(THEME_CONST);
-    const foundTheme = storedTheme ?? (isBrowserSchemeDark() ? DARK : LIGHT);
-    setDarkTheme(foundTheme === DARK);
-  }, []);
+  // useLayoutEffect(() => {
+  //   const storedTheme = localStorage.getItem(THEME_CONST);
+  //   const foundTheme = storedTheme ?? (isBrowserSchemeDark() ? DARK : LIGHT);
+  //   setDarkTheme(foundTheme === DARK);
+  // }, []);
 
-  useEffect(() => {
-    if (isDark === null) {
-      return;
-    }
-    if (isDark) {
-      localStorage.setItem(THEME_CONST, DARK);
-      document.documentElement.classList.add(DARK);
-      document.body.classList.add("bg-gray-darker");
-    } else {
-      localStorage.setItem(THEME_CONST, LIGHT);
-      document.documentElement.classList.remove(DARK);
-      document.body.classList.remove("bg-gray-darker");
-      document.body.classList.add("bg-gray-bg-light");
-    }
-  }, [isDark]);
+  // useEffect(() => {
+  //   if (isDark === null) {
+  //     return;
+  //   }
+  //   if (isDark) {
+  //     localStorage.setItem(THEME_CONST, DARK);
+  //     document.documentElement.classList.add(DARK);
+  //     document.body.classList.add("bg-gray-darker");
+  //   } else {
+  //     localStorage.setItem(THEME_CONST, LIGHT);
+  //     document.documentElement.classList.remove(DARK);
+  //     document.body.classList.remove("bg-gray-darker");
+  //     document.body.classList.add("bg-gray-bg-light");
+  //   }
+  // }, [isDark]);
 
-  const toggleTheme = useCallback(() => {
-    setDarkTheme(!isDark);
-  }, [setDarkTheme, isDark]);
+  // const toggleTheme = useCallback(() => {
+  //   setDarkTheme(!isDark);
+  // }, [setDarkTheme, isDark]);
 
   return (
-    <UseTheme.Provider value={{ isDark, toggleTheme }}>
+    <UseTheme.Provider value={{ isDark: false, toggleTheme: () => {} }}>
       {children}
     </UseTheme.Provider>
   );
 };
 
-const isBrowserSchemeDark = () =>
-  window.matchMedia?.("(prefers-color-scheme: dark)").matches;
+// const isBrowserSchemeDark = () =>
+//   window.matchMedia?.("(prefers-color-scheme: dark)").matches;
 
 const useTheme = () => useContext(UseTheme);
 
