6d4142bb705520a88106f8364e8474dc36abd22b
Greg <niebadz@gmail.com>
2024-04-25 12:12:05 +0200

    useRedirectWhenSigned

--- apps/frontend/app/(website)/studio/src/store/zustand/userSession.ts
+++ apps/frontend/app/(website)/studio/src/store/zustand/userSession.ts
@@ -1,72 +1,63 @@
-import {
-	type Output,
-	array,
-	literal,
-	nullable,
-	object,
-	string,
-	union,
-} from "valibot";
-
 import { uniq, without } from "ramda";
 import { create } from "zustand";
 import { persist } from "zustand/middleware";
 import type { ToVoid } from "../../types/transformations";
 
-export const userSessionSchema = object({
-	pendingActionsWhenSigned: array(union([literal("openRepoModal")])),
-	codemodExecutionId: union([nullable(string())]),
-});
+export const pendingActions = [
+  "openRepoModal",
+  "redirectToStudio",
+  "redirectToRegister",
+] as const;
+export type PendingAction = (typeof pendingActions)[number];
 
-type UserSession = Output<typeof userSessionSchema>;
-
-export const Actions = ["openRepoModal"];
-
-export type PendingAction = UserSession["pendingActionsWhenSigned"][number];
-
-export type UserSessionStore = UserSession & {
-	setPendingActions: ToVoid<PendingAction[]>;
-	setCodemodExecutionId: ToVoid<string | null>;
-	addPendingActionsWhenSigned: ToVoid<PendingAction>;
-	retrievePendingAction: (action: PendingAction) => boolean;
-	hasPendingAction: (action: PendingAction) => boolean;
-	resetPendingActions: VoidFunction;
+type UserSessionGet = {
+  pendingActionsWhenSigned: PendingAction[];
+  codemodExecutionId: string | null;
 };
 
-const buildDefaultUserSession = (): UserSession => ({
-	pendingActionsWhenSigned: [],
-	codemodExecutionId: null,
+export type UserSessionStore = UserSessionGet & {
+  setPendingActions: ToVoid<PendingAction[]>;
+  setCodemodExecutionId: ToVoid<string | null>;
+  addPendingActionsWhenSigned: ToVoid<PendingAction>;
+  retrievePendingAction: (action: PendingAction) => boolean;
+  hasPendingAction: (action: PendingAction) => boolean;
+  resetPendingActions: VoidFunction;
+};
+
+const buildDefaultUserSession = (): UserSessionGet => ({
+  pendingActionsWhenSigned: [],
+  codemodExecutionId: null,
 });
 
 export const useUserSession = create<UserSessionStore>()(
-	persist(
-		(set, get) => ({
-			...buildDefaultUserSession(),
-			setCodemodExecutionId: (codemodExecutionId: string | null) => {
-				set({ codemodExecutionId });
-			},
-			setPendingActions: (actions: PendingAction[]) => {
-				const uniqActions = uniq(actions);
-				set({ pendingActionsWhenSigned: uniqActions });
-			},
-			hasPendingAction: (action: PendingAction) =>
-				get().pendingActionsWhenSigned.includes(action),
-			addPendingActionsWhenSigned: (action: PendingAction) =>
-				get().setPendingActions(get().pendingActionsWhenSigned.concat(action)),
-			retrievePendingAction: (action: PendingAction) => {
-				const {
-					pendingActionsWhenSigned,
-					setPendingActions: setPendingAction,
-				} = get();
-				const hasAction = pendingActionsWhenSigned.includes(action);
-				if (hasAction)
-					setPendingAction(without([action], pendingActionsWhenSigned));
-				return hasAction;
-			},
-			resetPendingActions: () => get().setPendingActions([]),
-		}),
-		{
-			name: "userSession",
-		},
-	),
+  persist(
+    (set, get) => ({
+      ...buildDefaultUserSession(),
+      setCodemodExecutionId: (codemodExecutionId: string | null) => {
+        set({ codemodExecutionId });
+      },
+      setPendingActions: (actions: PendingAction[]) => {
+        const uniqActions = uniq(actions);
+        set({ pendingActionsWhenSigned: uniqActions });
+      },
+      hasPendingAction: (action: PendingAction) =>
+        get().pendingActionsWhenSigned.includes(action),
+      addPendingActionsWhenSigned: (action: PendingAction) =>
+        get().setPendingActions(get().pendingActionsWhenSigned.concat(action)),
+      retrievePendingAction: (action: PendingAction) => {
+        const {
+          pendingActionsWhenSigned,
+          setPendingActions: setPendingAction,
+        } = get();
+        const hasAction = pendingActionsWhenSigned.includes(action);
+        if (hasAction)
+          setPendingAction(without([action], pendingActionsWhenSigned));
+        return hasAction;
+      },
+      resetPendingActions: () => get().setPendingActions([]),
+    }),
+    {
+      name: "userSession",
+    },
+  ),
 );
