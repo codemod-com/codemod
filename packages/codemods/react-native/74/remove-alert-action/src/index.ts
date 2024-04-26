85198425641441f8b52a6e6d4a0a8562fbf54b18
Aleksy Rybicki <alekso.php@gmail.com>
2024-04-26 13:19:00 +0100

    React native 0.74 codemods

---
+++ packages/codemods/react-native/74/remove-alert-action/src/index.ts
@@ -0,0 +1,107 @@
+/*! @license
+The MIT License (MIT)
+
+Copyright (c) 2023 Codemod.com
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+*/
+
+/*
+Changes to the original file: added TypeScript, dirty flag, nullability checks
+*/
+
+import type {
+  API,
+  ASTPath,
+  CallExpression,
+  FileInfo,
+  Options,
+} from "jscodeshift";
+
+export default function transform(
+  file: FileInfo,
+  api: API,
+  options?: Options,
+): string | undefined {
+  const j = api.jscodeshift;
+  const root = j(file.source);
+
+  const variablesToRemove = [] as string[];
+
+  const removeAlertAction = (path: ASTPath<CallExpression>) => {
+    // Check if the argument is an object
+    if (path.node.arguments[0]?.type === "ObjectExpression") {
+      // Find the property 'alertAction' and remove it
+      path.node.arguments[0].properties =
+        path.node.arguments[0].properties.filter((property) => {
+          if (
+            property.type === "Property" &&
+            property.value.type === "Identifier"
+          ) {
+            variablesToRemove.push(property.value.name);
+          }
+
+          if ("key" in property && property.key && "name" in property.key) {
+            return property.key.name !== "alertAction";
+          }
+
+          return true;
+        });
+    }
+  };
+  root
+    .find(j.CallExpression, {
+      callee: {
+        property: {
+          name: "presentLocalNotification",
+        },
+      },
+    })
+    .forEach(removeAlertAction);
+
+  root
+    .find(j.CallExpression, {
+      callee: {
+        property: {
+          name: "scheduleLocalNotification",
+        },
+      },
+    })
+    .forEach(removeAlertAction);
+
+  // Remove variables if they are not used anywhere else
+  variablesToRemove
+    .filter((variable) => {
+      const isUsed = !!root
+        .find(j.Identifier, { name: variable })
+        .filter((path) => {
+          return (
+            path.parent?.value.type !== "VariableDeclarator" ||
+            path.parent?.value.id.name !== variable
+          );
+        }).length;
+
+      return !isUsed;
+    })
+    .forEach((variable) => {
+      root.find(j.VariableDeclarator, { id: { name: variable } }).remove();
+    });
+
+  return root.toSource();
+}
