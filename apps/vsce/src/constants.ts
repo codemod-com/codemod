6a5e381ba1e62c9efdfb7ccb5ccd73f4a8987b99
Benny Joo <sldisek783@gmail.com>
2024-05-01 13:37:16 +0100

    [monorepo] create a constants.ts file at the root level

--- apps/vsce/src/constants.ts
+++ apps/vsce/src/constants.ts
@@ -1,7 +1,8 @@
+export {
+  VSCODE_PREFIX,
+  CURSOR_PREFIX,
+  ACCESS_TOKEN_REQUESTED_BY_VSCE_STORAGE_KEY,
+  ACCESS_TOKEN_REQUESTED_BY_CURSOR_STORAGE_KEY,
+} from "../../../constants";
+
 export const CLI_NPM_LINK = "https://registry.npmjs.org/codemod/latest";
-export const ACCESS_TOKEN_REQUESTED_BY_VSCE_SEARCH_PARAM =
-  "accessTokenRequestedByVSCE";
-export const ACCESS_TOKEN_REQUESTED_BY_CURSOR_SEARCH_PARAM =
-  "accessTokenRequestedByCursor";
-export const VSCODE_PREFIX = "vscode://";
-export const CURSOR_PREFIX = "cursor://";
---
+++ constants.ts
@@ -0,0 +1,6 @@
+export const VSCODE_PREFIX = "vscode//";
+export const CURSOR_PREFIX = "cursor://";
+export const ACCESS_TOKEN_REQUESTED_BY_VSCE_STORAGE_KEY =
+  "accessTokenRequestedByVSCE";
+export const ACCESS_TOKEN_REQUESTED_BY_CURSOR_STORAGE_KEY =
+  "accessTokenRequestedByCursor";
