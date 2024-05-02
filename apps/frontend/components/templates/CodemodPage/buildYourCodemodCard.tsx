8fa261fc7f3ba869cc3de085be7d2e9afca7bf47
Grzegorz <niebadz@gmail.com>
2024-05-02 19:20:06 +0200

    Registry/fix/multiple made for (#484)

---
+++ apps/frontend/components/templates/CodemodPage/buildYourCodemodCard.tsx
@@ -0,0 +1,26 @@
+import LinkButton from "@/components/shared/LinkButton";
+import type { CodemodPagePayload } from "@/types";
+
+export const getCodemodCard = (data: CodemodPagePayload | null) =>
+  data?.globalLabels?.cta?.link && (
+    <div className="relative flex w-full flex-col gap-s overflow-clip rounded-[8px] border border-border-light p-s dark:border-border-dark">
+      {data?.globalLabels?.ctaTitle && (
+        <div className="relative z-10 flex flex-col gap-[12px]">
+          <p className="xs-heading">
+            {data?.globalLabels?.ctaTitle || "Build custom codemods"}
+          </p>
+          {data?.globalLabels?.ctaDescription && (
+            <p className="body-s">{data?.globalLabels?.ctaDescription}</p>
+          )}
+        </div>
+      )}
+      <img
+        className="pointer-events-none absolute left-0 top-[60px] -z-10 w-full"
+        alt="background illustration"
+        src="/illustration/planet.svg"
+      />
+      <LinkButton href={data?.globalLabels?.cta?.link} intent="primary">
+        {data?.globalLabels?.cta?.label || "Get started now"}
+      </LinkButton>
+    </div>
+  );
---
+++ apps/frontend/components/templates/CodemodPage/getFrameworkCard.tsx
@@ -0,0 +1,51 @@
+import { SanityImage } from "@/components/shared/SanityImage";
+import Tag from "@/components/shared/Tag";
+import type { FilterIcon } from "@/components/templates/Registry/helpers";
+import { REGISTRY_FILTER_TYPES } from "@/constants";
+import { capitalize } from "@utils/strings";
+import Link from "next/link";
+
+export const getFrameworkCard = ({
+  name: framework,
+  image: frameworkImage,
+}: { name: string; image: FilterIcon }) => (
+  <Link
+    key={framework}
+    href={`/registry?${
+      REGISTRY_FILTER_TYPES.framework
+    }=${framework.toLowerCase()}`}
+    prefetch
+  >
+    <Tag intent="default">
+      <>
+        {frameworkImage?.image.light && (
+          <SanityImage
+            maxWidth={20}
+            image={frameworkImage.image.light}
+            alt={frameworkImage.image.light.alt}
+            elProps={{
+              width: 20,
+              height: 20,
+              className: "h-5 w-5 dark:hidden",
+            }}
+          />
+        )}
+
+        {frameworkImage?.image.dark && (
+          <SanityImage
+            maxWidth={20}
+            image={frameworkImage.image.dark}
+            alt={frameworkImage.image.dark.alt}
+            elProps={{
+              width: 20,
+              height: 20,
+              className: "hidden h-5 w-5 dark:inline",
+            }}
+          />
+        )}
+      </>
+
+      <span className="capitalize">{capitalize(framework)}</span>
+    </Tag>
+  </Link>
+);
