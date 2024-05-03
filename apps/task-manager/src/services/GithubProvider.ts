5cde19993cf1335c781c2cad6451f73b234f5106
mirai2k <mikhail.demidoff@gmail.com>
2024-05-03 16:32:11 +0300

    wip: removed auth in git clone

--- apps/task-manager/src/services/GithubProvider.ts
+++ apps/task-manager/src/services/GithubProvider.ts
@@ -41,11 +41,10 @@ export class GithubProviderService {
       const { repoUrl, token } = this.__codemodMetadata;
       const { authorName, repoName } = parseGithubRepoUrl(repoUrl);
 
-      const config = [`http.extraHeader=AUTHORIZATION: token ${token}`];
-      const url = `https://${token}:x-oauth-basic@github.com/${authorName}/${repoName}.git`;
+      const config = [`http.extraHeader=Authorization: token ${token}`];
 
       const git = simpleGit({ config });
-      await git.clone(url, path);
+      await git.clone(`https://github.com/${authorName}/${repoName}.git`, path);
       this.__git = simpleGit(path, { config });
     } catch (error) {
       const { message } = error as Error;
