4912be85da0c6ead6abb39de65e4d000a4d799f3
Dmytro Hryshyn <dmytro@codemod.com>
2024-05-09 13:58:47 +0300

    add replace react dom codemod

---
+++ packages/codemods/react/19/replace-reactdom-render/test/test.ts
@@ -0,0 +1,47 @@
+import assert from "node:assert";
+import { readFile } from "node:fs/promises";
+import { join } from "node:path";
+import jscodeshift, { type API } from "jscodeshift";
+import { describe, it } from "vitest";
+import transform from "../src/index.js";
+
+const buildApi = (parser: string | undefined): API => ({
+  j: parser ? jscodeshift.withParser(parser) : jscodeshift,
+  jscodeshift: parser ? jscodeshift.withParser(parser) : jscodeshift,
+  stats: () => {
+    console.error(
+      "The stats function was called, which is not supported on purpose",
+    );
+  },
+  report: () => {
+    console.error(
+      "The report function was called, which is not supported on purpose",
+    );
+  },
+});
+
+describe("react/19/replace-reactdom-render", () => {
+  it("replace reactdom.render with root.render and replace imports", async () => {
+    const INPUT = await readFile(
+      join(__dirname, "..", "__testfixtures__/fixture1.input.js"),
+      "utf-8",
+    );
+    const OUTPUT = await readFile(
+      join(__dirname, "..", "__testfixtures__/fixture1.output.js"),
+      "utf-8",
+    );
+
+    const actualOutput = transform(
+      {
+        path: "index.js",
+        source: INPUT,
+      },
+      buildApi("tsx"),
+    );
+
+    assert.deepEqual(
+      actualOutput?.replace(/W/gm, ""),
+      OUTPUT.replace(/W/gm, ""),
+    );
+  });
+});
