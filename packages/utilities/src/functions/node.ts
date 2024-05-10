015c40b77f8e0e63b4988ea3963ef4b178bb4781
Sasha Mysak <reactionnnnn@gmail.com>
2024-05-10 15:12:25 +0200

    unintentionally removed code addition

--- packages/utilities/src/functions/node.ts
+++ packages/utilities/src/functions/node.ts
@@ -5,3 +5,12 @@ export const execPromise = promisify(exec);
 
 export const sleep = (ms: number) =>
   new Promise((resolve) => setTimeout(resolve, ms));
+
+export const isGeneratorEmpty = async (
+  genFunc: () => AsyncGenerator<unknown> | Generator<unknown>,
+) => {
+  const tempGen = genFunc();
+  const { done } = await tempGen.next();
+
+  return done;
+};
