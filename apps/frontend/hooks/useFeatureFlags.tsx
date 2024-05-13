9f67d9a1b31f1c9d5431ec46d4bfc3610ce620a5
Benny Joo <sldisek783@gmail.com>
2024-05-14 02:33:12 +0900

    Add useFeatureFlags hook + Add it to github runs feature (#550)

---
+++ apps/frontend/hooks/useFeatureFlags.tsx
@@ -0,0 +1,15 @@
+import { FEATURE_FLAG_QUERY_KEY } from "@/utils/strings";
+import { useState } from "react";
+
+const useFeatureFlags = () => {
+  const [features] = useState(() => {
+    const urlParams = new URLSearchParams(window.location.search);
+
+    const featureFlags = urlParams.get(FEATURE_FLAG_QUERY_KEY) ?? "";
+    return featureFlags.split(",");
+  });
+
+  return features;
+};
+
+export default useFeatureFlags;
