77247cbed8a1e74ecc0aeeb396e71cee24eae4c7
Sasha Mysak <reactionnnnn@gmail.com>
2024-05-14 15:02:14 +0200

    add back studio url

--- packages/utilities/src/constants.ts
+++ packages/utilities/src/constants.ts
@@ -1,1 +1,2 @@
-export const CODEMOD_STUDIO_URL = "https://codemod.com";
+export const CODEMOD_STUDIO_URL = "https://codemod.com/studio";
+export const CODEMOD_HOME_PAGE_URL = "https://codemod.com";
