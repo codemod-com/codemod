9d6c9d502fc14c13b144d6121090cc23696310ca
Sasha Mysak <reactionnnnn@gmail.com>
2024-05-14 18:25:04 +0200

    working solution

--- apps/cli/src/safeArgumentRecord.ts
+++ apps/cli/src/safeArgumentRecord.ts
@@ -5,11 +5,12 @@ import {
   doubleQuotify,
   safeParseArgument,
 } from "@codemod-com/utilities";
+import inquirer from "inquirer";
 
-export const buildSafeArgumentRecord = (
+export const buildSafeArgumentRecord = async (
   codemod: Codemod,
   rawArgumentRecord: Record<string, unknown>,
-): ArgumentRecord => {
+): Promise<ArgumentRecord> => {
   if (codemod.source === "standalone") {
     // no checks performed for local codemods
     // b/c no source of truth for the arguments
@@ -29,83 +30,204 @@ export const buildSafeArgumentRecord = (
 
   const safeArgumentRecord: ArgumentRecord = {};
 
-  const invalid: ((typeof codemod.arguments)[number] & { err: string })[] = [];
+  let invalid: ((typeof codemod.arguments)[number] & { err: string })[] = [];
 
   const validateArg = (
     descriptor: (typeof codemod.arguments)[number],
-    arg: unknown,
+    value: unknown,
   ) => {
-    const maybeArgument = safeParseArgument(arg);
+    const validator = (
+      descriptor: (typeof codemod.arguments)[number],
+      arg: unknown,
+      skipInvalidFlagging = false,
+    ) => {
+      const maybeArgument = safeParseArgument(arg);
 
-    if (
-      !maybeArgument.success &&
-      descriptor.required &&
-      descriptor.default === undefined
-    ) {
-      invalid.push({ ...descriptor, err: "required but missing" });
-      return false;
-    }
+      if (
+        !maybeArgument.success &&
+        descriptor.required &&
+        descriptor.default === undefined
+      ) {
+        if (!skipInvalidFlagging) {
+          invalid.push({ ...descriptor, err: "required but missing" });
+        }
+        return false;
+      }
 
-    if (!maybeArgument.success) {
-      return false;
-    }
+      if (!maybeArgument.success) {
+        return false;
+      }
 
-    if (descriptor.kind === "enum") {
-      if (!descriptor.options.includes(maybeArgument.output)) {
-        invalid.push({
-          ...descriptor,
-          err: `incorrect value. Valid options: ${descriptor.options.join(
-            ",",
-          )}`,
-        });
-      } else {
+      if (descriptor.kind === "enum") {
+        if (!descriptor.options.includes(maybeArgument.output)) {
+          if (!skipInvalidFlagging) {
+            invalid.push({
+              ...descriptor,
+              err: `incorrect value. Valid options: ${descriptor.options.join(
+                ",",
+              )}`,
+            });
+          }
+
+          return false;
+        }
+
         safeArgumentRecord[descriptor.name] = maybeArgument.output;
+        return true;
+      }
+
+      if (descriptor.kind === "boolean") {
+        if (
+          maybeArgument.output === "true" ||
+          maybeArgument.output === "false"
+        ) {
+          if (!skipInvalidFlagging) {
+            invalid.push({
+              ...descriptor,
+              err: "incorrect value. Valid options: true, false",
+            });
+          }
+
+          return false;
+        }
+
+        safeArgumentRecord[descriptor.name] = maybeArgument.output === "true";
+        return true;
+      }
+
+      if (descriptor.kind === "number") {
+        const numArg = Number(maybeArgument.output);
+        if (Number.isNaN(numArg) || !Number.isFinite(numArg)) {
+          if (!skipInvalidFlagging) {
+            invalid.push({ ...descriptor, err: "invalid number" });
+          }
+
+          return false;
+        }
+
+        safeArgumentRecord[descriptor.name] = numArg;
+        return true;
+      }
+
+      if (
+        descriptor.kind === "string" &&
+        typeof maybeArgument.output === "string"
+      ) {
+        if (maybeArgument.output.length === 0) {
+          if (!skipInvalidFlagging) {
+            invalid.push({ ...descriptor, err: "empty string" });
+          }
+
+          return false;
+        }
+
+        safeArgumentRecord[descriptor.name] = maybeArgument.output;
+        return true;
+      }
+
+      if (
+        // biome-ignore lint: incorrect warning
+        typeof maybeArgument.output === descriptor.kind
+      ) {
+        safeArgumentRecord[descriptor.name] = maybeArgument.output;
+      } else if (descriptor.default !== undefined) {
+        safeArgumentRecord[descriptor.name] = descriptor.default;
       }
 
       return true;
-    }
+    };
 
-    if (
-      // biome-ignore lint: incorrect warning
-      typeof maybeArgument.output === descriptor.kind
-    ) {
-      safeArgumentRecord[descriptor.name] = maybeArgument.output;
-    } else if (descriptor.default !== undefined) {
-      safeArgumentRecord[descriptor.name] = descriptor.default;
-    }
-
-    return true;
-  };
-
-  codemod.arguments.forEach((descriptor) => {
     if (Array.isArray(descriptor.kind)) {
-      let isValid = false;
-
       for (const kind of descriptor.kind) {
-        const valid = validateArg(
-          { ...descriptor, kind } as any,
-          rawArgumentRecord[descriptor.name],
-        );
+        const valid = validator({ ...descriptor, kind } as any, value, true);
 
         if (valid) {
-          isValid = true;
-          break;
+          return true;
         }
       }
 
-      return;
+      invalid.push({
+        ...descriptor,
+        err: `incorrect type or enum value`,
+      });
+      return false;
     }
 
-    return validateArg(descriptor, rawArgumentRecord[descriptor.name]);
-  });
+    return validator(descriptor, value);
+  };
+
+  codemod.arguments.forEach((descriptor) =>
+    validateArg(descriptor, rawArgumentRecord[descriptor.name]),
+  );
 
   if (invalid.length > 0) {
-    const missingString = `- ${invalid
+    const answers = await inquirer.prompt<ArgumentRecord>(
+      invalid.map((arg) => {
+        const isEnum = arg.kind === "enum";
+
+        if (isEnum) {
+          return {
+            type: "list",
+            name: arg.name,
+            choices: arg.options,
+            default: arg.options[0],
+            pageSize: arg.options.length,
+            message: `Please select a missing value for ${arg.name} argument`,
+          };
+        }
+
+        if (Array.isArray(arg.kind)) {
+          const kinds = [...arg.kind];
+
+          const enumElIdx = kinds.indexOf("enum");
+          if (enumElIdx !== -1) {
+            kinds.splice(enumElIdx, 1);
+          }
+
+          let message = `Please enter a missing value for ${
+            arg.name
+          } argument (${kinds.join(", ")}`;
+
+          if ("options" in arg) {
+            message += ` or one of the following: ${arg.options.join(", ")}`;
+          }
+          message += ")";
+
+          return {
+            type: "input",
+            name: arg.name,
+            message,
+          };
+        }
+
+        return {
+          type: "input",
+          name: arg.name,
+          message: `Please enter a missing value for ${arg.name} argument (${arg.kind})`,
+        };
+      }),
+    );
+
+    invalid = [];
+
+    for (const [name, value] of Object.entries(answers)) {
+      const descriptor = codemod.arguments.find((arg) => arg.name === name);
+
+      if (!descriptor) {
+        continue;
+      }
+
+      validateArg(descriptor, value);
+    }
+  }
+
+  if (invalid.length > 0) {
+    const invalidString = `- ${invalid
       .map(({ kind, name, err }) => `${doubleQuotify(name)} (${kind}) - ${err}`)
       .join("\n- ")}`;
 
     throw new Error(
-      `Invalid arguments:\n${missingString}\n\nPlease provide missing values as "--<arg-name> <value>" or make sure the provided values are valid.`,
+      `Invalid arguments:\n${invalidString}\n\nMake sure provided values are correct.`,
     );
   }
 
