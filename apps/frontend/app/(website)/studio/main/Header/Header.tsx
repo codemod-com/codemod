1381aea79ba53d3b9ca2eb36dd04f258c2f1ef9b
Benny Joo <sldisek783@gmail.com>
2024-05-17 16:11:50 +0100

    Revert "[studio] Remove feature flag for github run feature"
    This reverts commit c47ccd7dcb310530a0cbd19cffb0e9c0b56a5db2.

--- apps/frontend/app/(website)/studio/main/Header/Header.tsx
+++ apps/frontend/app/(website)/studio/main/Header/Header.tsx
@@ -1,16 +1,20 @@
+import useFeatureFlags from "@/hooks/useFeatureFlags";
+import { CODEMOD_RUN_FEATURE_FLAG } from "@/utils/strings";
 import { CodemodButton } from "@studio/components/CodemodButton";
 import { DownloadZip } from "../DownloadZip";
 import { TopBar } from "./TopBar";
 import { HeaderButtons } from "./headerButtons";
 
 export const Header = () => {
+  const ffs = useFeatureFlags();
+
   return (
     <>
       <TopBar />
       <div className="flex justify-between items-center h-[40px] w-full p-1 px-4">
         <div />
         <div className="flex gap-2 items-center">
-          <CodemodButton />
+          {ffs.includes(CODEMOD_RUN_FEATURE_FLAG) && <CodemodButton />}
           <HeaderButtons />
           <DownloadZip />
         </div>
