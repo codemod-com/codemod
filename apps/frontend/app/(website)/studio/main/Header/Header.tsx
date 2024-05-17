80e9202348d9935ea315c3147d92f4f844a6a29f
Benny Joo <sldisek783@gmail.com>
2024-05-17 13:23:41 +0100

    [studio] Remove feature flag for github run feature

--- apps/frontend/app/(website)/studio/main/Header/Header.tsx
+++ apps/frontend/app/(website)/studio/main/Header/Header.tsx
@@ -1,20 +1,16 @@
-import useFeatureFlags from "@/hooks/useFeatureFlags";
-import { CODEMOD_RUN_FEATURE_FLAG } from "@/utils/strings";
 import { CodemodButton } from "@studio/components/CodemodButton";
 import { DownloadZip } from "../DownloadZip";
 import { TopBar } from "./TopBar";
 import { HeaderButtons } from "./headerButtons";
 
 export const Header = () => {
-  const ffs = useFeatureFlags();
-
   return (
     <>
       <TopBar />
       <div className="flex justify-between items-center h-[40px] w-full p-1 px-4">
         <div />
         <div className="flex gap-2 items-center">
-          {ffs.includes(CODEMOD_RUN_FEATURE_FLAG) && <CodemodButton />}
+          <CodemodButton />
           <HeaderButtons />
           <DownloadZip />
         </div>
