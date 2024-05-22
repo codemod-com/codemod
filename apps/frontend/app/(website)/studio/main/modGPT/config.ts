44ef3d8c4baae0d8c4b7325a82974fa370dee800
Grzegorz <niebadz@gmail.com>
2024-05-22 18:02:38 +0200

    codemodAI communication (#679)

--- apps/frontend/app/(website)/studio/main/modGPT/config.ts
+++ apps/frontend/app/(website)/studio/main/modGPT/config.ts
@@ -1,10 +1,12 @@
-import { isDevelopment } from "@/config";
 import {
   CODEMOD_AI_FEATURE_FLAG,
+  CODEMOD_MOD_SERVICE_FEATURE_FLAG,
   FEATURE_FLAG_QUERY_KEY,
 } from "@/utils/strings";
 import { isServer } from "@studio/config";
 import { SEND_CHAT } from "@studio/constants";
+
+export const isDevelopment = process.env.NODE_ENV === "development";
 
 export const shouldUseCodemodAi = isServer
   ? false
@@ -13,14 +15,24 @@ export const shouldUseCodemodAi = isServer
       ?.split(",")
       .includes(CODEMOD_AI_FEATURE_FLAG);
 
-if (shouldUseCodemodAi) console.info("Experimental AI services active");
+export const shouldUseModService = isServer
+  ? false
+  : new URLSearchParams(window?.location.search)
+      .get(FEATURE_FLAG_QUERY_KEY)
+      ?.split(",")
+      .includes(CODEMOD_MOD_SERVICE_FEATURE_FLAG);
+
+if (shouldUseCodemodAi) console.info("Experimental AI service active");
+if (shouldUseModService) console.info("Experimental mogGPT service active");
+
 export const codemodAiWsServer = isDevelopment
   ? "ws://127.0.0.1:8000/ws"
   : "wss://backend.codemod.com/ws";
 
-const prodGptServer = shouldUseCodemodAi
+const prodGptServer = shouldUseModService
   ? "https://backend.codemod.com/modgpt"
   : "https://backend.codemod.com";
+
 export const modGptServer = `${
   isDevelopment ? "http://0.0.0.0:8082" : prodGptServer
 }/${SEND_CHAT}`;
