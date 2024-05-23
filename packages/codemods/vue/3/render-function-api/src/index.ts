10e9546a37ac5211474fa0a4855dfee8e5c86477
DmytroHryshyn <dmytro@codemod.com>
2024-05-23 13:39:07 +0300

    [Registry]: vue/3/render-function-api (#686)
    * fix
    
    * version

--- packages/codemods/vue/3/render-function-api/src/index.ts
+++ packages/codemods/vue/3/render-function-api/src/index.ts
@@ -5,32 +5,31 @@ const transform: Transform = (file: FileInfo, api: API, options: Options) => {
   const root = j(file.source);
 
   let dirtyFlag = false;
-
-  // Add import statement for 'h' from 'vue'
-  const importStatement = j.importDeclaration(
-    [j.importSpecifier(j.identifier("h"))],
-    j.literal("vue"),
-  );
-
-  root.get().node.program.body.unshift(importStatement);
-  dirtyFlag = true;
+  let hName: string | null = null;
 
   // Find the render method and update it
   root.find(j.ObjectMethod, { key: { name: "render" } }).forEach((path) => {
-    const params = path.value.params;
-    if (
-      params.length === 1 &&
-      params[0].type === "Identifier" &&
-      params[0].name === "h"
-    ) {
+    const firstParam = path.value.params.at(0);
+
+    if (j.Identifier.check(firstParam)) {
+      hName = firstParam.name;
+
       path.value.params = [];
       dirtyFlag = true;
     }
   });
 
-  if (!dirtyFlag) {
+  if (!dirtyFlag || hName === null) {
     return undefined;
   }
+
+  // Add import statement for 'h' from 'vue'
+  const importStatement = j.importDeclaration(
+    [j.importSpecifier(j.identifier(hName))],
+    j.literal("vue"),
+  );
+
+  root.get().node.program.body.unshift(importStatement);
 
   return root.toSource(options);
 };
