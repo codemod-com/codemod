46e5feb3b99efe2214ed9b7cf1b72cdb5501be9a
Dmytro Hryshyn <dmytro@codemod.com>
2024-05-27 20:16:39 +0300

    refactor codemod

--- packages/codemods/react/19/replace-reactdom-render/src/index.ts
+++ packages/codemods/react/19/replace-reactdom-render/src/index.ts
@@ -1,37 +1,39 @@
 import type {
   API,
+  ASTPath,
+  CallExpression,
   Collection,
   FileInfo,
   JSCodeshift,
-  Options,
 } from "jscodeshift";
 
-const findMethodCalls = (
-  j: JSCodeshift,
-  root: Collection<any>,
-  methodName: string,
-  namedImportLocalName: string,
-  defaultImportName: string,
-) =>
-  root.find(j.CallExpression).filter((path) => {
+const getMatcher =
+  (
+    j: JSCodeshift,
+    importSpecifiersLocalNames: string[],
+    importedModuleName: string,
+  ) =>
+  (path: ASTPath<CallExpression>) => {
     const { callee } = path.value;
 
-    if (j.Identifier.check(callee) && callee.name === namedImportLocalName) {
-      return true;
+    if (
+      j.Identifier.check(callee) &&
+      importSpecifiersLocalNames.includes(callee.name)
+    ) {
+      return callee.name;
     }
 
     if (
       j.MemberExpression.check(callee) &&
       j.Identifier.check(callee.object) &&
-      callee.object.name === defaultImportName &&
-      j.Identifier.check(callee.property) &&
-      callee.property.name === methodName
+      callee.object.name === importedModuleName &&
+      j.Identifier.check(callee.property)
     ) {
-      return true;
+      return callee.property.name;
     }
 
-    return false;
-  });
+    return null;
+  };
 
 const getImportDeclaration = (
   j: JSCodeshift,
@@ -77,132 +79,156 @@ const addNamedImport = (
   }
 };
 
-export default function transform(
-  file: FileInfo,
-  api: API,
-  options?: Options,
-): string | undefined {
-  const j = api.jscodeshift;
-  const root = j(file.source);
+const collectImportNames = (
+  j: JSCodeshift,
+  root: Collection,
+  source: string,
+) => {
+  const importSpecifierLocalNames = new Map<string, string>();
 
-  const reactNamedImportNamesToLocalNamesMap = new Map<string, string>();
+  let importDefaultSpecifierName: string | null = null;
+  let importNamespaceSpecifierName: string | null = null;
 
-  let reactDomDefaultImportName: string | null = null;
-
-  let isDirty = false;
   root
     .find(j.ImportDeclaration, {
-      source: { value: "react-dom" },
+      source: { value: source },
     })
     .forEach((path) => {
       path.value.specifiers?.forEach((specifier) => {
-        // named import
         if (j.ImportSpecifier.check(specifier)) {
-          reactNamedImportNamesToLocalNamesMap.set(
+          importSpecifierLocalNames.set(
             specifier.imported.name,
             specifier.local?.name ?? "",
           );
         }
 
-        // default and wildcard import
-        if (
-          j.ImportDefaultSpecifier.check(specifier) ||
-          j.ImportNamespaceSpecifier.check(specifier)
-        ) {
-          reactDomDefaultImportName = specifier.local?.name ?? null;
+        if (j.ImportDefaultSpecifier.check(specifier) && specifier.local) {
+          importDefaultSpecifierName = specifier.local.name;
+        }
+
+        if (j.ImportNamespaceSpecifier.check(specifier) && specifier.local) {
+          importNamespaceSpecifierName = specifier.local.name;
         }
       });
     });
 
-  /**
-   * replace unmountComponentAtNode
-   */
+  return {
+    importSpecifierLocalNames,
+    importDefaultSpecifierName,
+    importNamespaceSpecifierName,
+  };
+};
 
-  findMethodCalls(
+const replaceHydrate = (
+  j: JSCodeshift,
+  root: Collection,
+  path: ASTPath<CallExpression>,
+) => {
+  const args = path.value.arguments;
+
+  const hydrateRoot = j.expressionStatement(
+    j.callExpression(j.identifier("hydrateRoot"), [args[1], args[0]]),
+  );
+
+  addNamedImport(j, root, "hydrateRoot", "react-dom/client");
+  path.parent.replace(hydrateRoot);
+};
+
+const replaceRender = (
+  j: JSCodeshift,
+  root: Collection,
+  path: ASTPath<CallExpression>,
+) => {
+  const args = path.value.arguments;
+
+  const createRoot = j.variableDeclaration("const", [
+    j.variableDeclarator(
+      j.identifier("root"),
+      j.callExpression(j.identifier("createRoot"), [args[1]]),
+    ),
+  ]);
+
+  const render = j.expressionStatement(
+    j.callExpression(
+      j.memberExpression(j.identifier("root"), j.identifier("render")),
+      [args[0]],
+    ),
+  );
+
+  addNamedImport(j, root, "createRoot", "react-dom/client");
+
+  path.parent.replace(createRoot);
+  path.parent.insertAfter(render);
+};
+
+const replaceUnmountComponentAtNode = (
+  j: JSCodeshift,
+  root: Collection,
+  path: ASTPath<CallExpression>,
+) => {
+  const args = path.value.arguments;
+
+  const createRoot = j.variableDeclaration("const", [
+    j.variableDeclarator(
+      j.identifier("root"),
+      j.callExpression(j.identifier("createRoot"), [args[0]]),
+    ),
+  ]);
+
+  const unmount = j.expressionStatement(
+    j.callExpression(
+      j.memberExpression(j.identifier("root"), j.identifier("unmount")),
+      [],
+    ),
+  );
+
+  addNamedImport(j, root, "createRoot", "react-dom/client");
+
+  path.parent.replace(createRoot);
+  path.parent.insertAfter(unmount);
+};
+
+const replacementFunctions: Record<string, (...args: any) => any> = {
+  render: replaceRender,
+  hydrate: replaceHydrate,
+  unmountComponentAtNode: replaceUnmountComponentAtNode,
+};
+
+export default function transform(
+  file: FileInfo,
+  api: API,
+): string | undefined {
+  const j = api.jscodeshift;
+  const root = j(file.source);
+
+  let isDirty = false;
+
+  const {
+    importNamespaceSpecifierName,
+    importDefaultSpecifierName,
+    importSpecifierLocalNames,
+  } = collectImportNames(j, root, "react-dom");
+
+  const importedModuleName =
+    importDefaultSpecifierName ?? importNamespaceSpecifierName ?? "";
+
+  const matchMethod = getMatcher(
     j,
-    root,
-    "unmountComponentAtNode",
-    reactNamedImportNamesToLocalNamesMap.get("unmountComponentAtNode") ?? "",
-    reactDomDefaultImportName ?? "",
-  ).forEach((path) => {
-    const args = path.value.arguments;
+    [...importSpecifierLocalNames.values()],
+    importedModuleName,
+  );
 
-    const createRoot = j.variableDeclaration("const", [
-      j.variableDeclarator(
-        j.identifier("root"),
-        j.callExpression(j.identifier("createRoot"), [args[0]]),
-      ),
-    ]);
+  root.find(j.CallExpression).forEach((path) => {
+    const name = matchMethod(path);
 
-    const unmount = j.expressionStatement(
-      j.callExpression(
-        j.memberExpression(j.identifier("root"), j.identifier("unmount")),
-        [],
-      ),
-    );
+    if (name === null) {
+      return;
+    }
 
-    addNamedImport(j, root, "createRoot", "react-dom/client");
+    const replaceMethod = replacementFunctions[name];
 
-    path.parent.replace(createRoot);
-    path.parent.insertAfter(unmount);
-
+    replaceMethod(j, root, path);
     isDirty = true;
-  });
-
-  /**
-   * replace ReactDOM.hydrate
-   */
-
-  findMethodCalls(
-    j,
-    root,
-    "hydrate",
-    reactNamedImportNamesToLocalNamesMap.get("hydrate") ?? "",
-    reactDomDefaultImportName ?? "",
-  ).forEach((path) => {
-    const args = path.value.arguments;
-
-    const hydrateRoot = j.expressionStatement(
-      j.callExpression(j.identifier("hydrateRoot"), [args[1], args[0]]),
-    );
-
-    isDirty = true;
-
-    addNamedImport(j, root, "hydrateRoot", "react-dom/client");
-    path.parent.replace(hydrateRoot);
-  });
-
-  /**
-   * replace ReactDOM.render
-   */
-  findMethodCalls(
-    j,
-    root,
-    "render",
-    reactNamedImportNamesToLocalNamesMap.get("render") ?? "",
-    reactDomDefaultImportName ?? "",
-  ).forEach((path) => {
-    const args = path.value.arguments;
-
-    const createRoot = j.variableDeclaration("const", [
-      j.variableDeclarator(
-        j.identifier("root"),
-        j.callExpression(j.identifier("createRoot"), [args[1]]),
-      ),
-    ]);
-
-    const render = j.expressionStatement(
-      j.callExpression(
-        j.memberExpression(j.identifier("root"), j.identifier("render")),
-        [args[0]],
-      ),
-    );
-
-    isDirty = true;
-    addNamedImport(j, root, "createRoot", "react-dom/client");
-
-    path.parent.replace(createRoot);
-    path.parent.insertAfter(render);
   });
 
   return isDirty ? root.toSource() : undefined;
