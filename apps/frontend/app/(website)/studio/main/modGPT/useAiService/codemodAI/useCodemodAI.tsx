f8221382859296302d4ae36ccc861d1f8c4b7796
Grzegorz <niebadz@gmail.com>
2024-05-27 16:57:50 +0200

    [studio] fix generate codemod button behaviour (#716)

--- apps/frontend/app/(website)/studio/main/modGPT/useAiService/codemodAI/useCodemodAI.tsx
+++ apps/frontend/app/(website)/studio/main/modGPT/useAiService/codemodAI/useCodemodAI.tsx
@@ -2,7 +2,8 @@ import { codemodAiWsServer, shouldUseCodemodAi } from "@chatbot/config";
 import type { LLMMessage, MessageFromWs, MessageToWs } from "@chatbot/types";
 import type { LLMEngine } from "@shared/consts";
 import { useSnippetStore } from "@studio/store/zustand/snippets";
-import { type Dispatch, type SetStateAction, useEffect, useState } from "react";
+import { useEffect, useState } from "react";
+import toast from "react-hot-toast";
 
 export const useCodemodAI = ({
   messages,
@@ -16,28 +17,37 @@ export const useCodemodAI = ({
   const { inputSnippet: before, afterSnippet: after } = useSnippetStore();
   const [serviceBusy, setServiceBusy] = useState(false);
 
+  const handleError = (error: string) => {
+    setServiceBusy(false);
+    toast.error(`WebSocket Error ${error}`);
+  };
   useEffect(() => {
     if (!shouldUseCodemodAi) return;
     const websocket = new WebSocket(codemodAiWsServer);
     websocket.onopen = () => console.info("WebSocket connection established");
     websocket.onmessage = async (event) => {
       const data = JSON.parse(event.data) as MessageToWs;
-      if (data.codemod) {
-        setServiceBusy(true);
+      if (data.error || data.execution_status === "error") {
+        handleError(data.error || "server crashed");
+      } else if (data.codemod) {
         setWsMessage({
           codemod: data.codemod,
           content: `\`\`\`ts ${data.codemod}\`\`\``,
           role: "assistant",
           id: Date.now().toString(),
         });
-      } else
+        setServiceBusy(false);
+      } else {
         setWsMessage({
           content: data.message,
           role: "assistant",
           id: Date.now().toString(),
         });
+      }
     };
-    websocket.onerror = (error) => console.log("WebSocket Error:", error);
+    websocket.onerror = (error) => {
+      handleError(String(error));
+    };
     setWs(websocket);
 
     return () => {
@@ -46,7 +56,7 @@ export const useCodemodAI = ({
   }, []);
 
   const startIterativeCodemodGeneration = () => {
-    if (ws && ws.readyState === WebSocket.OPEN && serviceBusy) {
+    if (ws && ws.readyState === WebSocket.OPEN && !serviceBusy) {
       const messageToSend = JSON.stringify({
         config: { llm_engine: engine },
         previous_context: messages,
@@ -54,11 +64,12 @@ export const useCodemodAI = ({
         after,
       });
       ws.send(messageToSend);
-      setServiceBusy(false);
+      setServiceBusy(true);
     }
   };
 
   return {
+    setServiceBusy,
     startIterativeCodemodGeneration,
     wsMessage,
     serviceBusy,
