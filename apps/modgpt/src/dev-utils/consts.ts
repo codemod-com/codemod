74be95d099bb09fd92b40f17427694c37451d97a
Greg <niebadz@gmail.com>
2024-05-28 17:57:39 +0200

    modgpt llmEngines updated

--- apps/modgpt/src/dev-utils/consts.ts
+++ apps/modgpt/src/dev-utils/consts.ts
@@ -1,9 +1,5 @@
+import { llmEngines } from "../../../shared";
+
 export const port = process?.env?.PORT || "9999";
-export const engines = [
-  "gpt-4-with-chroma",
-  "gpt-4",
-  "claude-2.0",
-  "claude-instant-1.2",
-  "replit-code-v1-3b",
-];
-export const roles = ["system", "user", "assistant"];
+export const engines = llmEngines;
+export const roles = ["system", "user", "assistant", "function"];
