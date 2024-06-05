22f97d1171de011ea9fef295e49de7aeb3f213a0
Greg <niebadz@gmail.com>
2024-06-05 22:15:09 +0200

    fix blur on tour

--- apps/frontend/app/(website)/studio/features/FirstLoginExperience/FirstLoginExperience.tsx
+++ apps/frontend/app/(website)/studio/features/FirstLoginExperience/FirstLoginExperience.tsx
@@ -1,29 +1,26 @@
-import Button from "@/components/shared/Button";
 import { ExplainIcon } from "@features/FirstLoginExperience/ExplainIcon";
 import { getIntroJsOptions } from "@features/FirstLoginExperience/config";
-import { Icon, Lightbulb } from "@phosphor-icons/react";
-import { isServer } from "@studio/config";
+import { Lightbulb } from "@phosphor-icons/react";
 import { Steps } from "intro.js-react";
 import "intro.js/introjs.css";
 import { useEffect, useState } from "react";
 
-export const FirstLoginExperience = ({ className }: { className: string }) => {
-  const isIntro =
-    !isServer && !!document.getElementsByClassName("introjs-helperLayer")[0];
-  const [isEnebaled, setIsEnabled] = useState(false);
+export const FirstLoginExperience = () => {
+  const [isEnabled, setIsEnabled] = useState(false);
+  const onStart = () => {
+    setIsEnabled(true);
+    (
+      document.getElementsByClassName("sign-in-required")[0] as HTMLElement
+    ).style.display = "none";
+  };
   useEffect(() => {
     const isFirstTimeUser = !localStorage.getItem("returningUser");
     localStorage?.setItem("returningUser", "true");
     if (isFirstTimeUser) {
       sessionStorage.setItem("isFirstTime", "true");
-      setTimeout(() => {
-        setIsEnabled(true);
-        (
-          document.getElementsByClassName("sign-in-required")[0] as HTMLElement
-        ).style.display = "none";
-      }, 4000);
+      setTimeout(onStart, 4000);
     }
-  }, [isIntro]);
+  }, []);
 
   const onEnd = () => {
     (
@@ -36,8 +33,9 @@ export const FirstLoginExperience = ({ className }: { className: string }) => {
     <>
       <Steps
         initialStep={0}
-        enabled={isEnebaled}
+        enabled={isEnabled}
         onComplete={onEnd}
+        onExit={onEnd}
         showStepNumbers={true}
         options={{
           nextLabel: "Next",
@@ -45,13 +43,12 @@ export const FirstLoginExperience = ({ className }: { className: string }) => {
           doneLabel: "Start your adventure!",
         }}
         steps={getIntroJsOptions().steps}
-        onExit={onEnd}
       />
       <div
         className="width-[50px]
            hover:text-accent-foreground rounded-md hover:bg-accent
            flex items-center justify-center cursor-pointer group mr-2 px-3"
-        onClick={() => setIsEnabled(true)}
+        onClick={onStart}
       >
         <div className="flex flex-col items-center">
           <ExplainIcon text={"Explain the app"} Icon={Lightbulb} />
