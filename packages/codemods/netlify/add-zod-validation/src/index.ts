00aad6b6be4b980b65f288c61393d3f25cce2b80
Aleksy Rybicki <alekso.php@gmail.com>
2024-06-05 12:10:54 +0100

    added more complex case

--- packages/codemods/netlify/add-zod-validation/src/index.ts
+++ packages/codemods/netlify/add-zod-validation/src/index.ts
@@ -45,28 +45,50 @@ You have to:
   }).ai`
 Previously query parameters were not typed.
 return {
-  api,
-  param1: query.param1,
-  someOtherParam: query.some_other_param,
-  andSoOnParams: query.and_so_on_params,
-  user: selectCurrentUser(state),
+api,
+param1: query.param1,
+someOtherParam: query.some_other_param,
+andSoOnParams: query.and_so_on_params,
+user: selectCurrentUser(state),
 }
 
 Now we are using zod to type query parameters:
 import { z } from 'zod';
 const querySchema = z.object({
-  'param1': z.string(),
-  'some_other_param': z.string(),
-  'and_so_on_params': z.string(),
+'param1': z.string(),
+'some_other_param': z.string(),
+'and_so_on_params': z.string(),
 });
 const parsedQuery = querySchema.parse(query);
 return {
-  api,
-  param1: parsedQuery.param1,
-  someOtherParam: parsedQuery.some_other_param,
-  andSoOnParams: parsedQuery.and_so_on_params,
-  user: selectCurrentUser(state),
+api,
+param1: parsedQuery.param1,
+someOtherParam: parsedQuery.some_other_param,
+andSoOnParams: parsedQuery.and_so_on_params,
+user: selectCurrentUser(state),
 }
+
+You have to:
+1. Add zod import statement.
+2. Add zod schema for query parameters.
+3. Parse query using zod schema.
+4. Remove optional chaining and use parsed query instead.
+5. Add new line between lines of code.
+`;
+
+  await jsFiles().astGrep("const { $$$_ } = buildQuery($$$_)").ai`
+Previously query parameters were not typed:
+const { param1, another_param } = buildQuery(ownProps.location.search);
+
+Now we are using zod to type query parameters:
+import { z } from 'zod;
+const querySchema = z.object({
+  param1: z.string(),
+  another_param: z.string(), 
+})
+const query = buildQuery(ownProps.location.search);
+const parsedQuery = querySchema.parse(query);
+const { search, page } = parsedQuery
 
 You have to:
 1. Add zod import statement.
