e538f59fc76968689103d6162c92087ce69c316e
Aleksy Rybicki <alekso.php@gmail.com>
2024-06-05 11:58:33 +0100

    added new use case

--- packages/codemods/netlify/add-zod-validation/__testfixtures__/fixture1.input.ts
+++ packages/codemods/netlify/add-zod-validation/__testfixtures__/fixture1.input.ts
@@ -1,2 +1,2 @@
 const query = buildQuery(props.location?.search ?? '');
-const someParam = query?.['some-param'] === 'true';
+import { z } from 'zod';const querySchema = z.object({  'some-param': z.string(),});const parsedQuery = querySchema.parse(query);const someParam = parsedQuery['some-param'] === 'true';
--- packages/codemods/netlify/add-zod-validation/src/index.ts
+++ packages/codemods/netlify/add-zod-validation/src/index.ts
@@ -32,4 +32,47 @@ You have to:
 4. Remove optional chaining and use parsed query instead.
 5. Add new line between lines of code.
 `;
+
+  await jsFiles().astGrep({
+    rule: {
+      pattern: "return { $$$_ }",
+      regex: "query\\.",
+      follows: {
+        pattern: "const query = buildQuery($$$_)",
+        stopBy: "end",
+      },
+    },
+  }).ai`
+Previously query parameters were not typed.
+return {
+  api,
+  param1: query.param1,
+  someOtherParam: query.some_other_param,
+  andSoOnParams: query.and_so_on_params,
+  user: selectCurrentUser(state),
+}
+
+Now we are using zod to type query parameters:
+import { z } from 'zod';
+const querySchema = z.object({
+  'param1': z.string(),
+  'some_other_param': z.string(),
+  'and_so_on_params': z.string(),
+});
+const parsedQuery = querySchema.parse(query);
+return {
+  api,
+  param1: parsedQuery.param1,
+  someOtherParam: parsedQuery.some_other_param,
+  andSoOnParams: parsedQuery.and_so_on_params,
+  user: selectCurrentUser(state),
+}
+
+You have to:
+1. Add zod import statement.
+2. Add zod schema for query parameters.
+3. Parse query using zod schema.
+4. Remove optional chaining and use parsed query instead.
+5. Add new line between lines of code.
+`;
 }
