76073016aa3585745c4b68c45dc77e28ba49f31a
Benny Joo <sldisek783@gmail.com>
2024-06-15 13:14:34 +0100

    fix CSP error

--- apps/frontend/middleware.ts
+++ apps/frontend/middleware.ts
@@ -23,7 +23,7 @@ export async function middleware(request: NextRequest) {
     const nonce = Buffer.from(crypto.randomUUID()).toString("base64");
     const cspHeader = `
         default-src 'self';
-        script-src 'self' 'unsafe-inline' https://summary-walrus-25.clerk.accounts.dev https://clerk.codemod.com https://vercel.live https://cdn.jsdelivr.net https://www.googletagmanager.com;
+        script-src 'self' 'unsafe-eval' 'unsafe-inline' https://summary-walrus-25.clerk.accounts.dev https://clerk.codemod.com https://vercel.live https://cdn.jsdelivr.net https://www.googletagmanager.com;
 		    frame-src https://challenges.cloudflare.com/ https://vercel.live;
         connect-src *.google-analytics.com https://clerk.codemod.com https://api.short.io https://backend.codemod.com https://codemod.com https://vitals.vercel-insights.com https://summary-walrus-25.clerk.accounts.dev https://*.vercel.app https://vercel.live wss://backend.codemod.com wss://*.api.sanity.io;
         style-src 'self' 'unsafe-inline' https://vercel.live;
