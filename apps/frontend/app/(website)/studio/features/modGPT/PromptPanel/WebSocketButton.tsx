01dbbe843e2f8ed28d6a17b4bf4d89d23e5380c9
Greg <niebadz@gmail.com>
2024-06-17 13:34:46 +0200

    two buttons

--- apps/frontend/app/(website)/studio/features/modGPT/PromptPanel/WebSocketButton.tsx
+++ apps/frontend/app/(website)/studio/features/modGPT/PromptPanel/WebSocketButton.tsx
@@ -1,6 +1,8 @@
 import type { useAiService } from "@chatbot/useAiService";
 import type { useCodemodAI } from "@chatbot/useAiService/codemodAI/useCodemodAI";
+import ButtonWithTooltip from "@studio/components/button/BottonWithTooltip";
 import { Button } from "@studio/components/ui/button";
+import Link from "next/link";
 
 export const WebSocketButton = ({
   handleButtonClick,
@@ -12,13 +14,27 @@ export const WebSocketButton = ({
   isLoading: ReturnType<typeof useAiService>["isLoading"];
 }) => {
   return (
-    <Button
+    <ButtonWithTooltip
+      tooltipContent={
+        <>
+          with selected model and Codemod’s iterative AI system.
+          <Link
+            style={{ color: "blue" }}
+            href="https://codemod.com/blog/iterative-ai-system"
+          >
+            {" "}
+            Learn more
+          </Link>
+        </>
+      }
+      variant="default"
       size="sm"
-      className="group my-0 h-8 whitespace-nowrap !py-0 text-xs font-bold bg-accent"
+      className="text-white flex gap-1 text-xs my-0 h-8 !py-0 bg-black hover:bg-accent hover:text-black"
+      // className="group my-0 h-8 whitespace-nowrap !py-0 text-xs font-bold bg-primary"
       onClick={handleButtonClick}
       disabled={isLoading}
     >
-      Generate codemod with AI
-    </Button>
+      Autogenerate with Codemod AI (BETA)
+    </ButtonWithTooltip>
   );
 };
