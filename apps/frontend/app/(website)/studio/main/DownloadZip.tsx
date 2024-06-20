04984524d3bf5f380082f29efb5f955c412bb335
Sasha Mysak <reactionnnnn@gmail.com>
2024-06-20 13:56:05 +0200

    wip

--- apps/frontend/app/(website)/studio/main/DownloadZip.tsx
+++ apps/frontend/app/(website)/studio/main/DownloadZip.tsx
@@ -1,6 +1,6 @@
 import { cn } from "@/utils";
 import { useAuth, useSession } from "@clerk/nextjs";
-import sendMessage from "@studio/api/sendMessage";
+import { getHumanCodemodName } from "@studio/api/getHumanCodemodName";
 import { Button } from "@studio/components/ui/button";
 import {
   Dialog,
@@ -20,20 +20,6 @@ import { useSnippetsStore } from "@studio/store/snippets";
 import { downloadProject } from "@studio/utils/download";
 import { Check, Copy } from "lucide-react";
 import { useMemo, useState } from "react";
-
-export const generateCodemodHumanNamePrompt = (codemod: string) => `
-You are a jscodeshift codemod and javascript expert. 
-Come up with a precise name to be used for the following jscodeshift codemod below.
-If the codemod is aimed at making any changes to a particular framework or library, the format
-should be "framework/version/name", where framework is the name of the framework or library,
-version is a major version (meaning one or two digits), and name is a short name for the codemod
-written in kebab-case. If you can't determine which framework this is for, you can just return the name
-written in kebab-case.
-Do not return any text other than the codemod name.
-\`\`\`
-${codemod}
-\`\`\`
-`;
 
 export const DownloadZip = () => {
   const [isOpen, setIsOpen] = useState(false);
@@ -82,7 +68,7 @@ export const DownloadZip = () => {
       <DialogTrigger asChild>
         <Button
           size="xs"
-          variant="default"
+          variant="unstyled"
           className="text-white flex gap-1"
           hint={
             <p className="font-normal">
@@ -167,40 +153,4 @@ export function CopyTerminalCommands({ text }: { text: string }) {
       </Button>
     </div>
   );
-}
-
-async function getHumanCodemodName(
-  codemod: string,
-  token: string | null,
-): Promise<string> {
-  if (token === null) {
-    return "codemod";
-  }
-
-  try {
-    if (!codemod) {
-      throw new Error("codemod content not found");
-    }
-
-    let codemodName = "";
-    if (token !== null) {
-      // Ask LLM to come up with a name for the given codemod
-      const codemodNameOrError = await sendMessage({
-        message: generateCodemodHumanNamePrompt(codemod),
-        token,
-      });
-
-      if (codemodNameOrError.isLeft()) {
-        console.error(codemodNameOrError.getLeft());
-      } else {
-        codemodName = codemodNameOrError.get().text;
-      }
-    }
-
-    return codemodName;
-  } catch (error) {
-    console.error(error);
-
-    return "codemod";
-  }
 }
--- apps/frontend/app/(website)/studio/main/RunOptions.tsx
+++ apps/frontend/app/(website)/studio/main/RunOptions.tsx
@@ -1,76 +1,139 @@
-import * as DropdownMenu from "@radix-ui/react-dropdown-menu";
-import { Button } from "@studio/components/ui/button";
-import { Separator } from "@studio/components/ui/separator";
+import { useAuth, useSession } from "@clerk/nextjs";
 import {
-  ChevronDownIcon,
-  ChevronUpIcon,
-  PlayIcon,
-  UploadIcon,
-} from "lucide-react";
-import Link from "next/link";
-import { usePathname, useRouter } from "next/navigation";
-import { useEffect, useState } from "react";
+  getCodemodProjectFiles,
+  isTypeScriptProjectFiles,
+} from "@codemod-com/utilities";
+import { getHumanCodemodName } from "@studio/api/getHumanCodemodName";
+import { Button } from "@studio/components/ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@studio/components/ui/dropdown-menu";
+import { Separator } from "@studio/components/ui/separator";
+import { ToastAction } from "@studio/components/ui/toast";
+import { useToast } from "@studio/components/ui/use-toast";
+import { useModStore } from "@studio/store/zustand/mod";
+import { useSnippetStore } from "@studio/store/zustand/snippets";
+import initSwc, { transform } from "@swc/wasm-web";
+import { ChevronDownIcon, ChevronUpIcon, PlayIcon } from "lucide-react";
+import { useState } from "react";
 import { DownloadZip } from "./DownloadZip";
 
 export const RunOptions = () => {
   const [open, setOpen] = useState(false);
+  const [isPublishing, setIsPublishing] = useState(false);
 
-  function handleClick(event: React.MouseEvent) {
-    event.preventDefault();
-    setOpen((prev) => !prev);
-  }
+  const modStore = useModStore();
+  const { engine, inputSnippet, afterSnippet } = useSnippetStore();
 
-  console.log("open", open);
+  const { session } = useSession();
+  const { getToken } = useAuth();
+
+  const { toast } = useToast();
+
+  const handleClick = async () => {
+    setIsPublishing(true);
+
+    if (!session) {
+      return toast({
+        variant: "destructive",
+        title: "Please first log in to use this feature",
+        action: <ToastAction altText="Goto login page">Log in</ToastAction>,
+      });
+    }
+    if (!modStore.internalContent) {
+      return;
+    }
+
+    const token = await getToken();
+
+    const humanCodemodName = await getHumanCodemodName(
+      modStore.internalContent,
+      token,
+    );
+
+    const files = getCodemodProjectFiles({
+      name: humanCodemodName,
+      codemodBody: modStore.internalContent,
+      cases: [{ before: inputSnippet, after: afterSnippet }],
+      engine,
+      username: session.user.username ?? session.user.fullName,
+    });
+
+    // Pre-built file
+    if (isTypeScriptProjectFiles(files)) {
+      await initSwc();
+      const { code: compiled } = await transform(files["src/index.ts"], {
+        minify: true,
+        module: { type: "commonjs" },
+        jsc: {
+          target: "es5",
+          loose: false,
+          parser: { syntax: "typescript", tsx: true },
+        },
+      });
+
+      // zip.file(
+      //   "dist/index.cjs",
+      //   `/*! @license\n${files.LICENSE}\n*/\n${compiled}`,
+      // );
+    }
+
+    setIsPublishing(false);
+    setIsOpen(true);
+  };
 
   return (
-    <DropdownMenu.Root open={open} modal={false}>
-      <DropdownMenu.Trigger
-        className="select-none py-px flex gap-1"
-        name="Run button"
-        aria-label="Click for run options"
+    <div className="rounded-md bg-black flex gap-1 h-7 items-center relative overflow-hidden">
+      <Button
+        size="xs"
+        variant="outline"
+        className="text-white flex gap-1 bg-inherit border-none hover:bg-gray-700 pl-4 z-10 peer"
+        hint={
+          <p className="font-normal">
+            Will publish the codemod to the Codemod Registry
+          </p>
+        }
+        isLoading={isPublishing}
+        disabled={!modStore.internalContent || isPublishing}
         onClick={handleClick}
+        id="run-codemod-button"
       >
-        <Button
-          size="xs"
-          variant="default"
-          className="text-white flex gap-1 bg-black"
-          // hint={
-          //   <p className="font-normal">
-          //     Will publish the codemod to the Codemod Registry
-          //   </p>
-          // }
-          // isLoading={isDownloading}
-          // disabled={!modStore.internalContent || isDownloading}
-          onClick={handleClick}
-          id="run-codemod-button"
-        >
-          <PlayIcon className="w-3" />
-          Run via CLI
-          <Separator orientation="vertical" className="mx-2 h-2/3" />
-          <Button variant="unstyled" role="list" className="p-0">
+        <PlayIcon className="w-3" />
+        Run via CLI
+      </Button>
+
+      <Separator orientation="vertical" className="mx-2 h-2/3 z-10" />
+
+      <DropdownMenu open={open} modal={false}>
+        <DropdownMenuTrigger asChild>
+          <Button
+            variant="unstyled"
+            size="xs"
+            role="list"
+            className="p-0 pr-4 z-10"
+            onClick={(e) => {
+              setOpen((prev) => !prev);
+            }}
+          >
             {open ? (
-              <ChevronUpIcon className="text-white w-3 mr-1" />
+              <ChevronUpIcon className="text-white w-3" />
             ) : (
-              <ChevronDownIcon className="text-white w-3 mr-1" />
+              <ChevronDownIcon className="text-white w-3" />
             )}
           </Button>
-        </Button>
-      </DropdownMenu.Trigger>
+        </DropdownMenuTrigger>
 
-      <DropdownMenu.Content
-        align="start"
-        side="bottom"
-        sideOffset={16}
-        // onMouseLeave={handleMouseLeave}
-        onCloseAutoFocus={(event) => event.preventDefault()}
-        onEscapeKeyDown={() => setOpen(false)}
-        onPointerDownOutside={() => setOpen(false)}
-        className="z-[99] min-w-[250px] animate-slideDownAndFade select-none rounded-[8px] border-[1px] border-border-light bg-primary-dark p-s shadow-sm dark:border-border-dark dark:bg-primary-light dark:shadow-none"
-      >
-        <DropdownMenu.Item asChild>
-          <DownloadZip />
-        </DropdownMenu.Item>
-      </DropdownMenu.Content>
-    </DropdownMenu.Root>
+        <DropdownMenuContent>
+          <DropdownMenuItem className="h-7 bg-white">
+            <DownloadZip />
+          </DropdownMenuItem>
+        </DropdownMenuContent>
+      </DropdownMenu>
+
+      <div className="absolute inset-0 bg-transparent peer-hover:bg-gray-700 transition-colors" />
+    </div>
   );
 };
