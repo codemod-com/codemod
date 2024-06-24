2d302c4e04b757e7f25f1dcc8f6b23ad8cb9eebe
mirai2k <mikhail.demidoff@gmail.com>
2024-06-24 13:13:42 +0300

    returned auth hook for modgpt

--- apps/modgpt/src/routes/sendChat.ts
+++ apps/modgpt/src/routes/sendChat.ts
@@ -31,54 +31,61 @@ const openAiEdgeApi = new openAiEdge.OpenAIApi(
 );
 
 export const getSendChatPath = (instance: Instance) =>
-  instance.post("/sendChat", async (request, reply) => {
-    const { messages, engine } = parseSendChatBody(request.body);
-    if (!messages[0]) {
-      return reply.code(400).send();
-    }
-
-    let completion: string | ChatMessage | null = null;
-    try {
-      if (engine === "claude-2.0" || engine === "claude-instant-1.2") {
-        completion = await claudeService.complete(engine, messages[0].content);
-      } else if (engine === "replit-code-v1-3b") {
-        completion = await replicateService.complete(messages[0].content);
-      } else if (engine === "gpt-4-with-chroma") {
-        const prompt = messages
-          .map(({ content, role }) => `${role}: ${content}`)
-          .join("\n");
-        completion = await chatGptApi.sendMessage(prompt);
-      } else if (openAiEdgeApi) {
-        const response = await openAiEdgeApi.createChatCompletion({
-          ...COMPLETION_PARAMS,
-          model: engine,
-          messages: messages.map((msg) => ({
-            role: msg.role,
-            content: msg.content,
-          })),
-          stream: true,
-        });
-        const headers = corsDisableHeaders;
-
-        const stream = OpenAIStream(response);
-        reply.raw.writeHead(200, headers);
-        reply.hijack();
-        const reader = stream.getReader();
-        await pushStreamToReply(reader, reply.raw);
-      } else {
-        throw new Error(
-          "You need to provide the OPEN_AI_API_KEY to use this endpoint",
-        );
+  instance.post(
+    "/sendChat",
+    { preHandler: instance.authenticate },
+    async (request, reply) => {
+      const { messages, engine } = parseSendChatBody(request.body);
+      if (!messages[0]) {
+        return reply.code(400).send();
       }
 
-      if (!reply.sent && completion) {
-        reply.type("text/plain; charset=utf-8").send(completion);
+      let completion: string | ChatMessage | null = null;
+      try {
+        if (engine === "claude-2.0" || engine === "claude-instant-1.2") {
+          completion = await claudeService.complete(
+            engine,
+            messages[0].content,
+          );
+        } else if (engine === "replit-code-v1-3b") {
+          completion = await replicateService.complete(messages[0].content);
+        } else if (engine === "gpt-4-with-chroma") {
+          const prompt = messages
+            .map(({ content, role }) => `${role}: ${content}`)
+            .join("\n");
+          completion = await chatGptApi.sendMessage(prompt);
+        } else if (openAiEdgeApi) {
+          const response = await openAiEdgeApi.createChatCompletion({
+            ...COMPLETION_PARAMS,
+            model: engine,
+            messages: messages.map((msg) => ({
+              role: msg.role,
+              content: msg.content,
+            })),
+            stream: true,
+          });
+          const headers = corsDisableHeaders;
+
+          const stream = OpenAIStream(response);
+          reply.raw.writeHead(200, headers);
+          reply.hijack();
+          const reader = stream.getReader();
+          await pushStreamToReply(reader, reply.raw);
+        } else {
+          throw new Error(
+            "You need to provide the OPEN_AI_API_KEY to use this endpoint",
+          );
+        }
+
+        if (!reply.sent && completion) {
+          reply.type("text/plain; charset=utf-8").send(completion);
+        }
+      } catch (error) {
+        console.error(error);
+        reply.send(error);
       }
-    } catch (error) {
-      console.error(error);
-      reply.send(error);
-    }
-  });
+    },
+  );
 
 async function pushStreamToReply(
   reader: ReadableStreamDefaultReader,
