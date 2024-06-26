b49e8e1e0a3269215efd5d140e88ac26a8b6b892
Dmytro Hryshyn <dmytro@codemod.com>
2024-06-26 16:22:37 +0300

    wip

--- packages/codemods/react/19/remove-legacy-context/src/index.ts
+++ packages/codemods/react/19/remove-legacy-context/src/index.ts
@@ -5,62 +5,14 @@ import type {
   ASTPath,
   ClassDeclaration,
   Collection,
+  Expression,
   FileInfo,
   JSCodeshift,
   Options,
+  ReturnStatement,
 } from "jscodeshift";
 
-const getMatcher =
-  (
-    j: JSCodeshift,
-    importSpecifiersLocalNames: string[],
-    importedModuleName: string,
-  ) =>
-  (path: ASTPath<ClassDeclaration>) => {
-    const { superClass } = path.value;
-
-    let component = null;
-
-    if (
-      j.Identifier.check(superClass) &&
-      importSpecifiersLocalNames.includes(superClass.name)
-    ) {
-      component = path.value;
-    }
-
-    if (
-      j.MemberExpression.check(superClass) &&
-      j.Identifier.check(superClass.object) &&
-      superClass.object.name === importedModuleName &&
-      j.Identifier.check(superClass.property)
-    ) {
-      component = path.value;
-    }
-
-    if (!component) {
-      return null;
-    }
-
-    const renderMethod =
-      component.body.body.find(
-        (node) =>
-          j.ClassMethod.check(node) &&
-          j.Identifier.check(node.key) &&
-          node.key.name === "render",
-      ) ?? null;
-
-    const renderedJsx = renderMethod
-      ? j(renderMethod).find(j.ReturnStatement).paths().at(0)
-      : null;
-
-    return {
-      component,
-      renderMethod,
-      renderedJsx,
-    };
-  };
-
-const collectImportNames = (
+const analyzeImportedModule = (
   j: JSCodeshift,
   root: Collection,
   source: string,
@@ -100,7 +52,100 @@ const collectImportNames = (
   };
 };
 
-// @TODO helper get react component
+const getClassComponents = (j: JSCodeshift, root: Collection<any>) => {
+  const {
+    importNamespaceSpecifierName,
+    importDefaultSpecifierName,
+    importSpecifierLocalNames,
+  } = analyzeImportedModule(j, root, "react");
+
+  const REACT_CLASS_COMPONENT_SUPERCLASS_NAMES = ["PureComponent", "Component"];
+
+  const importedComponentSuperclassNames =
+    REACT_CLASS_COMPONENT_SUPERCLASS_NAMES.map((name) =>
+      importSpecifierLocalNames.get(name),
+    ).filter(Boolean);
+
+  const importedModuleName =
+    importNamespaceSpecifierName ?? importDefaultSpecifierName;
+
+  return root.find(j.ClassDeclaration).filter((path) => {
+    const superClass = path.value.superClass;
+
+    if (
+      j.Identifier.check(superClass) &&
+      importedComponentSuperclassNames.includes(superClass.name)
+    ) {
+      return true;
+    }
+
+    if (
+      j.MemberExpression.check(superClass) &&
+      j.Identifier.check(superClass.object) &&
+      superClass.object.name === importedModuleName &&
+      j.Identifier.check(superClass.property) &&
+      importedComponentSuperclassNames.includes(superClass.property.name)
+    ) {
+      return true;
+    }
+
+    return false;
+  });
+};
+
+const getClassMethod = (
+  j: JSCodeshift,
+  klass: ASTPath<ClassDeclaration>,
+  name: string,
+) =>
+  j(klass)
+    .find(j.ClassMethod, {
+      key: {
+        type: "Identifier",
+        name,
+      },
+    })
+    .paths()
+    .at(0) ?? null;
+
+const getClassProperty = (
+  j: JSCodeshift,
+  klass: ASTPath<ClassDeclaration>,
+  name: string,
+) =>
+  j(klass)
+    .find(j.ClassProperty, {
+      key: {
+        type: "Identifier",
+        name,
+      },
+    })
+    .paths()
+    .at(0) ?? null;
+
+//  const FooContext = React.createContext();
+const buildContextVariableDeclaration = (j: JSCodeshift) =>
+  j.variableDeclaration("const", [
+    j.variableDeclarator(
+      j.identifier("Context"),
+      j.callExpression(
+        j.memberExpression(
+          j.identifier("React"),
+          j.identifier("createContext"),
+        ),
+        [],
+      ),
+    ),
+  ]);
+
+const buildContextProvider = (j: JSCodeshift, value: any, renderedJsx: any) =>
+  j.jsxElement(
+    j.jsxOpeningElement(j.jsxIdentifier("Context"), [
+      j.jsxAttribute(j.jsxIdentifier("value"), j.jsxExpressionContainer(value)),
+    ]),
+    j.jsxClosingElement(j.jsxIdentifier("Context")),
+    [renderedJsx],
+  );
 
 export default function transform(
   file: FileInfo,
@@ -110,44 +155,42 @@ export default function transform(
   const j = api.jscodeshift;
   const root = j(file.source);
 
-  const {
-    importDefaultSpecifierName,
-    importNamespaceSpecifierName,
-    importSpecifierLocalNames,
-  } = collectImportNames(j, root, "react");
+  getClassComponents(j, root).forEach((path) => {
+    const childContextTypes = getClassProperty(j, path, "childContextTypes");
+    const getChildContext = getClassMethod(j, path, "childContextTypes");
 
-  const moduleName =
-    importNamespaceSpecifierName ?? importDefaultSpecifierName ?? "";
-
-  const matchReactClassComponent = getMatcher(
-    j,
-    [...importSpecifierLocalNames.values()],
-    moduleName,
-  );
-
-  j.find(j.ClassDeclaration).forEach((path) => {
-    const match = matchReactClassComponent(path);
-
-    if (match === null) {
+    if (childContextTypes === null || getChildContext === null) {
       return;
     }
 
-    const { component, renderMethod, renderedJsx } = match;
+    const childContextValue =
+      j(getChildContext).find(j.ReturnStatement).paths().at(0)?.value
+        .argument ?? null;
 
-    let returnValue = null;
-    component.body.body.forEach((node) => {
-      if (j.ClassMethod.check(node) && j.Identifier.check(node.key)) {
-        const name = node.key.name;
+    j(childContextTypes).remove();
 
-        if (name !== "getChildContext") {
-          returnValue =
-            j(node).find(j.ReturnStatement).paths().at(0)?.value ?? null;
-        }
-      }
-    });
+    // add Context variable declaration
+    const variableDeclaration = buildContextVariableDeclaration(j);
+    path.insertBefore(variableDeclaration);
 
-    if (returnValue) {
+    const render = getClassMethod(j, path, "render");
+
+    const renderReturnStatement = render
+      ? j(render).find(j.ReturnStatement).paths().at(0)
+      : null;
+
+    const renderedJsx = renderReturnStatement?.value.argument;
+
+    if (!renderedJsx) {
+      return;
     }
+
+    renderReturnStatement.value.argument = buildContextProvider(
+      j,
+      childContextValue,
+      renderedJsx,
+    );
   });
+
   return root.toSource();
 }
