69511b721b0ab705aa0667a7f20ea3748d454b5e
DmytroHryshyn <dmytro@codemod.com>
2024-06-28 16:08:16 +0300

    [codemods] add replace-legacy-context codemod (#888)
    * wip
    
    * wip
    
    * wip
    
    * add tests, split to analysis part and transformation
    
    * add isDirty flag
    
    * fix readme
    
    * pnpm lock

---
+++ packages/codemods/react/19/remove-legacy-context/__testfixtures__/class-component.input.tsx
@@ -0,0 +1,16 @@
+import PropTypes from 'prop-types';
+import React from 'react';
+
+class Parent extends React.Component {
+  static childContextTypes = {
+    foo: PropTypes.string.isRequired,
+  };
+
+  getChildContext() {
+    return { foo: 'bar' };
+  }
+
+  render() {
+    return <Child />;
+  }
+}
---
+++ packages/codemods/react/19/remove-legacy-context/src/index.ts
@@ -0,0 +1,78 @@
+// BUILT WITH https://codemod.studio
+
+import type { API, FileInfo, JSCodeshift } from "jscodeshift";
+import { findPatterns, getClassMethod } from "./analyze.js";
+
+//  const FooContext = React.createContext();
+const buildContextVariableDeclaration = (j: JSCodeshift) =>
+  j.variableDeclaration("const", [
+    j.variableDeclarator(
+      j.identifier("Context"),
+      j.callExpression(
+        j.memberExpression(
+          j.identifier("React"),
+          j.identifier("createContext"),
+        ),
+        [],
+      ),
+    ),
+  ]);
+
+const buildContextProvider = (j: JSCodeshift, value: any, renderedJsx: any) =>
+  j.jsxElement(
+    j.jsxOpeningElement(j.jsxIdentifier("Context"), [
+      j.jsxAttribute(j.jsxIdentifier("value"), j.jsxExpressionContainer(value)),
+    ]),
+    j.jsxClosingElement(j.jsxIdentifier("Context")),
+    [renderedJsx],
+  );
+
+export default function transform(
+  file: FileInfo,
+  api: API,
+): string | undefined {
+  const j = api.jscodeshift;
+  const root = j(file.source);
+
+  let isDirty = false;
+
+  findPatterns(j, root).forEach((pattern) => {
+    if (!pattern) {
+      return;
+    }
+    isDirty = true;
+
+    const { classComponent, getChildContext, childContextTypes } = pattern;
+
+    const childContextValue =
+      j(getChildContext).find(j.ReturnStatement).paths().at(0)?.value
+        .argument ?? null;
+
+    j(childContextTypes).remove();
+    j(getChildContext).remove();
+
+    // add Context variable declaration
+    const variableDeclaration = buildContextVariableDeclaration(j);
+    classComponent.insertBefore(variableDeclaration);
+
+    const render = getClassMethod(j, classComponent, "render");
+
+    const renderReturnStatement = render
+      ? j(render).find(j.ReturnStatement).paths().at(0)
+      : null;
+
+    const renderedJsx = renderReturnStatement?.value.argument;
+
+    if (!renderedJsx) {
+      return;
+    }
+
+    renderReturnStatement.value.argument = buildContextProvider(
+      j,
+      childContextValue,
+      renderedJsx,
+    );
+  });
+
+  return isDirty ? root.toSource() : undefined;
+}
