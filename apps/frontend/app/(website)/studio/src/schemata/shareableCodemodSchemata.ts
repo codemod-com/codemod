afdb96c8b626035e93cfceb2f259eae6bf535d5e
Greg <niebadz@gmail.com>
2024-07-03 15:09:23 +0200

    build fix

--- apps/frontend/app/(website)/studio/src/schemata/shareableCodemodSchemata.ts
+++ apps/frontend/app/(website)/studio/src/schemata/shareableCodemodSchemata.ts
@@ -1,6 +1,7 @@
 import { knownEnginesSchema } from "@codemod-com/utilities";
 import {
   type Output,
+  array,
   literal,
   number,
   object,
@@ -16,6 +17,8 @@ export const shareableCodemodSchemata = object({
   n: optional(string()), // codemod name
   b: optional(string()), // before snippet
   a: optional(string()), // after snippet
+  // bm: optional(array(string())), // multiple before snippets with names
+  // am: optional(array(string())), // multiple after snippets with names
   c: optional(string()), // codemod content
   m: optional(union([literal("learn"), literal("accessTokenRequested")])), // command
 });
