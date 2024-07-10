02e20b2eb5740c6af9a4ef18b4de7efef6d0573b
Sasha Mysak <reactionnnnn@gmail.com>
2024-07-10 15:57:20 +0200

    fix publish test

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,6 +1,8 @@
+import tsconfigPaths from "vite-tsconfig-paths";
 import { configDefaults, defineConfig } from "vitest/config";
 
 export default defineConfig({
+  plugins: [tsconfigPaths()],
   test: {
     include: [...configDefaults.include, "**/test/*.ts"],
     passWithNoTests: true,
