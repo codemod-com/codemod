54ee8ce8dd607c0d202eca94d40a5c11ef3d52a4
Sasha Mysak <reactionnnnn@gmail.com>
2024-07-16 13:10:33 +0200

    fix

--- apps/backend/src/server.ts
+++ apps/backend/src/server.ts
@@ -155,373 +155,8 @@ const routes: FastifyPluginCallback = (instance, _opts, done) => {
     return { data: {} };
   });
 
-  instance.get("/version", async (_, reply) => {
-    const packageJson = await import(
-      new URL("../package.json", import.meta.url).href,
-      { assert: { type: "json" } }
-    );
-    reply.type("application/json").code(200);
-    return { version: packageJson.default.version };
-  });
-
-  instance.get(
-    "/codemods/:slug",
-    wrapRequestHandlerMethod(getCodemodBySlugHandler),
-  );
-
-  instance.get("/codemods", wrapRequestHandlerMethod(getCodemodsHandler));
-
-  instance.get(
-    "/codemods/downloadLink",
-    wrapRequestHandlerMethod(getCodemodDownloadLink),
-  );
-
-  instance.get(
-    "/codemods/list",
-    wrapRequestHandlerMethod(getCodemodsListHandler),
-  );
-
-  instance.post(
-    "/validateAccessToken",
-    wrapRequestHandlerMethod(validationHandler),
-  );
-
-  instance.get("/diffs/:id", async (request, reply) => {
-    const { id } = parseGetCodeDiffParams(request.params);
-    const { iv: ivStr } = parseIv(request.query);
-
-    const key = Buffer.from(environment.ENCRYPTION_KEY, "base64url");
-    const iv = Buffer.from(ivStr, "base64url");
-
-    const codeDiff = await prisma.codeDiff.findUnique({
-      where: { id },
-    });
-
-    if (!codeDiff) {
-      reply.code(400).send();
-      return;
-    }
-
-    type BeforeAfterDiff = {
-      before: string;
-      after: string;
-    };
-
-    let diffs: BeforeAfterDiff[];
-    try {
-      diffs = JSON.parse(
-        decrypt(
-          "aes-256-cbc",
-          { key, iv },
-          Buffer.from(codeDiff.diffs, "base64url"),
-        ).toString(),
-      ) as BeforeAfterDiff[];
-    } catch (err) {
-      reply.code(400).send();
-      return;
-    }
-
-    reply.type("application/json").code(200);
-    return { diffs };
-  });
-
-  instance.post("/diffs", async (request, reply) => {
-    const body = parseDiffCreationBody(request.body);
-
-    const iv = randomBytes(16);
-    const key = Buffer.from(environment.ENCRYPTION_KEY, "base64url");
-
-    const codeDiff = await prisma.codeDiff.create({
-      data: {
-        name: body.name,
-        source: body.source,
-        diffs: encrypt(
-          "aes-256-cbc",
-          { key, iv },
-          Buffer.from(JSON.stringify(body.diffs)),
-        ).toString("base64url"),
-      },
-    });
-
-    reply.type("application/json").code(200);
-    return { id: codeDiff.id, iv: iv.toString("base64url") };
-  });
-
-  instance.delete("/revokeToken", wrapRequestHandlerMethod(revokeTokenHandler));
-
-  instance.get("/intents/:id", async (request, reply) => {
-    if (!auth) {
-      throw new Error("This endpoint requires auth configuration.");
-    }
-
-    const { id } = parseValidateIntentParams(request.params);
-    const { iv: ivStr } = parseIv(request.query);
-
-    const key = Buffer.from(environment.ENCRYPTION_KEY, "base64url");
-    const iv = Buffer.from(ivStr, "base64url");
-
-    const result = await prisma.userLoginIntent.findFirst({
-      where: {
-        id: decrypt(
-          "aes-256-cbc",
-          { key, iv },
-          Buffer.from(id, "base64url"),
-        ).toString(),
-      },
-    });
-
-    if (result === null) {
-      reply.code(400).send();
-      return;
-    }
-
-    if (result.token === null) {
-      reply.code(400).send();
-      return;
-    }
-
-    const decryptedToken = decrypt(
-      "aes-256-cbc",
-      { key, iv },
-      Buffer.from(result.token, "base64url"),
-    ).toString();
-
-    await prisma.userLoginIntent.delete({
-      where: { id: result.id },
-    });
-
-    reply.type("application/json").code(200);
-    return { token: decryptedToken };
-  });
-
-  instance.post("/intents", async (_request, reply) => {
-    if (!auth) {
-      throw new Error("This endpoint requires auth configuration.");
-    }
-
-    const result = await prisma.userLoginIntent.create({});
-
-    const key = Buffer.from(environment.ENCRYPTION_KEY, "base64url");
-    const iv = randomBytes(16);
-    const encryptedSessionId = encrypt(
-      "aes-256-cbc",
-      { key, iv },
-      Buffer.from(result.id),
-    ).toString("base64url");
-
-    reply.type("application/json").code(200);
-    return { id: encryptedSessionId, iv: iv.toString("base64url") };
-  });
-
-  instance.post("/sourceControl/:provider/issues", async (request, reply) => {
-    if (!auth) {
-      throw new Error("This endpoint requires auth configuration.");
-    }
-
-    const { provider } = parseCreateIssueParams(request.params);
-
-    const { repoUrl, title, body } = parseCreateIssueBody(request.body);
-
-    const accessToken = getCustomAccessToken(environment, request.headers);
-
-    if (accessToken === null) {
-      return reply.code(401).send();
-    }
-
-    const userId = await tokenService.findUserIdMetadataFromToken(
-      accessToken,
-      BigInt(Date.now()),
-      CLAIM_ISSUE_CREATION,
-    );
-
-    const oAuthToken = await auth.getOAuthToken(userId, provider);
-
-    const sourceControlProvider = getSourceControlProvider(
-      provider,
-      oAuthToken,
-      repoUrl,
-    );
-
-    const result = await sourceControl.createIssue(sourceControlProvider, {
-      title,
-      body,
-    });
-
-    reply.type("application/json").code(200);
-    return result;
-  });
-
-  done();
-};
-
-const protectedRoutes: FastifyPluginCallback = (instance, _opts, done) => {
-  if (areClerkKeysSet(environment)) {
-    const clerkOptions = {
-      publishableKey: environment.CLERK_PUBLISH_KEY,
-      secretKey: environment.CLERK_SECRET_KEY,
-      jwtKey: environment.CLERK_JWT_KEY,
-    };
-
-    instance.register(clerkPlugin, clerkOptions);
-  } else {
-    console.warn("No Clerk keys set. Authentication is disabled.");
-  }
-
-  const claudeService = new ClaudeService(
-    environment.CLAUDE_API_KEY ?? null,
-    1024,
-  );
-
-  const replicateService = new ReplicateService(
-    environment.REPLICATE_API_KEY ?? null,
-  );
-
-  instance.post(
-    "/buildAccessToken",
-    wrapRequestHandlerMethod(buildAccessTokenHandler),
-  );
-
-  instance.post("/sendMessage", async (request, reply) => {
-    if (areClerkKeysSet(environment)) {
-      const { userId } = getAuth(request);
-      if (!userId) {
-        return reply.code(401).send();
-      }
-    } else {
-      console.warn("No Clerk keys set. Authentication is disabled.");
-    }
-
-    if (chatGptApi === null) {
-      throw new Error("The Chat GPT API requires the authentication key");
-    }
-
-    const { message, parentMessageId } = parseSendMessageBody(request.body);
-
-    const options: {
-      parentMessageId?: string;
-    } = {};
-
-    if (parentMessageId) {
-      options.parentMessageId = parentMessageId;
-    }
-
-    const result = await chatGptApi.sendMessage(message, options);
-
-    reply.type("application/json").code(200);
-    return result;
-  });
-
-  instance.post("/sendChat", async (request, reply) => {
-    if (areClerkKeysSet(environment)) {
-      const { userId } = getAuth(request);
-      if (!userId) {
-        return reply.code(401).send();
-      }
-    } else {
-      console.warn("No Clerk keys set. Authentication is disabled.");
-    }
-
-    const { messages, engine } = parseSendChatBody(request.body);
-
-    if (!messages[0]) {
-      return reply.code(400).send();
-    }
-
-    if (engine === "claude-2.0" || engine === "claude-instant-1.2") {
-      const completion = await claudeService.complete(
-        engine,
-        messages[0].content,
-      );
-
-      reply.type("text/plain; charset=utf-8").code(200);
-
-      return completion ?? "";
-    }
-
-    if (engine === "replit-code-v1-3b") {
-      const completion = await replicateService.complete(messages[0].content);
-
-      reply.type("text/plain; charset=utf-8").code(200);
-
-      return completion ?? "";
-    }
-
-    if (engine === "gpt-4-with-chroma") {
-      // chat history is not supported. passing all messages as single prompt.
-      const prompt = messages
-        .map(({ content, role }) => `${role}: ${content}`)
-        .join("\n");
-
-      const completion = await chromaService.complete(prompt);
-
-      reply.type("text/plain; charset=utf-8").code(200);
-
-      return completion ?? "";
-    }
-
-    if (openAiEdgeApi === null) {
-      throw new Error(
-        "You need to provide the OPEN_AI_API_KEY to use this endpoint",
-      );
-    }
-
-    const response = await openAiEdgeApi.createChatCompletion({
-      ...COMPLETION_PARAMS,
-      messages: messages.slice(),
-      stream: true,
-    });
-
-    const stream = OpenAIStream(response);
-
-    // the following code is inspired by the implementation of the streamToResponse function
-    // available here: https://github.com/vercel-labs/ai/blob/164b33d963250a53bbaaabb7b68d143f81541a7a/packages/core/streams/streaming-text-response.ts#L22C17-L22C33
-
-    reply.hijack();
-
-    const replyHeaders = reply.getHeaders();
-
-    const headers: OutgoingHttpHeaders = {};
-
-    Object.keys(replyHeaders).forEach((key) => {
-      const value = replyHeaders[key];
-
-      if (value === undefined || typeof value === "number") {
-        return;
-      }
-
-      headers[key] = value;
-    });
-
-    headers["Content-Type"] = "text/plain; charset=utf-8";
-
-    reply.raw.writeHead(200, headers);
-
-    const reader = stream.getReader();
-
-    const writeToReplyRaw = async () => {
-      const { done, value } = await reader.read();
-
-      if (done) {
-        reply.raw.end();
-        return;
-      }
-
-      reply.raw.write(value);
-
-      await writeToReplyRaw();
-    };
-
-    await writeToReplyRaw();
-
-    return;
-  });
-
-  instance.post("/publish", wrapRequestHandlerMethod(publishHandler));
-
-  instance.post("/unpublish", wrapRequestHandlerMethod(unpublishHandler));
-
-  instance.get(
-    "/sourceControl/:provider/user/repos",
+  instance.get<{ Reply: { version: string } }>(
+    "/version",
     async (request, reply) => {
       const packageJson = await import(
         new URL("../package.json", import.meta.url).href,
@@ -553,76 +188,66 @@ const protectedRoutes: FastifyPluginCallback = (instance, _opts, done) => {
     getCodemodsListHandler,
   );
 
-  instance.get<{ Reply: { before: string; after: string } }>(
-    "/diffs/:id",
-    async (request, reply) => {
-      const { id } = parseGetCodeDiffParams(request.params);
-      const { iv: ivStr } = parseIv(request.query);
+  instance.get("/diffs/:id", async (request, reply) => {
+    const { id } = parseGetCodeDiffParams(request.params);
+    const { iv: ivStr } = parseIv(request.query);
 
-      const key = Buffer.from(environment.ENCRYPTION_KEY, "base64url");
-      const iv = Buffer.from(ivStr, "base64url");
+    const key = Buffer.from(environment.ENCRYPTION_KEY, "base64url");
+    const iv = Buffer.from(ivStr, "base64url");
 
-      const codeDiff = await prisma.codeDiff.findUnique({
-        where: { id },
-      });
+    const codeDiff = await prisma.codeDiff.findUnique({
+      where: { id },
+    });
 
-      if (!codeDiff) {
-        reply.code(400).send();
-        return;
-      }
+    if (!codeDiff) {
+      reply.code(400).send();
+      return;
+    }
 
-      let before: string;
-      let after: string;
-      try {
-        before = decryptWithIv(
+    type BeforeAfterDiff = {
+      before: string;
+      after: string;
+    };
+
+    let diffs: BeforeAfterDiff[];
+    try {
+      diffs = JSON.parse(
+        decryptWithIv(
           "aes-256-cbc",
           { key, iv },
-          Buffer.from(codeDiff.before, "base64url"),
-        ).toString();
-        after = decryptWithIv(
+          Buffer.from(codeDiff.diffs, "base64url"),
+        ).toString(),
+      ) as BeforeAfterDiff[];
+    } catch (err) {
+      reply.code(400).send();
+      return;
+    }
+
+    reply.type("application/json").code(200);
+    return { diffs };
+  });
+
+  instance.post("/diffs", async (request, reply) => {
+    const body = parseDiffCreationBody(request.body);
+
+    const iv = randomBytes(16);
+    const key = Buffer.from(environment.ENCRYPTION_KEY, "base64url");
+
+    const codeDiff = await prisma.codeDiff.create({
+      data: {
+        name: body.name,
+        source: body.source,
+        diffs: encryptWithIv(
           "aes-256-cbc",
           { key, iv },
-          Buffer.from(codeDiff.after, "base64url"),
-        ).toString();
-      } catch (err) {
-        reply.code(400).send();
-        return;
-      }
+          Buffer.from(JSON.stringify(body.diffs)),
+        ).toString("base64url"),
+      },
+    });
 
-      reply.type("application/json").code(200);
-      return { before, after };
-    },
-  );
-
-  instance.post<{ Reply: { id: string; iv: string } }>(
-    "/diffs",
-    async (request, reply) => {
-      const body = parseDiffCreationBody(request.body);
-
-      const iv = randomBytes(16);
-      const key = Buffer.from(environment.ENCRYPTION_KEY, "base64url");
-
-      const codeDiff = await prisma.codeDiff.create({
-        data: {
-          name: body.name,
-          source: body.source,
-          before: encryptWithIv(
-            "aes-256-cbc",
-            { key, iv },
-            Buffer.from(body.before),
-          ).toString("base64url"),
-          after: encryptWithIv(
-            "aes-256-cbc",
-            { key, iv },
-            Buffer.from(body.after),
-          ).toString("base64url"),
-        },
-      });
-
-      reply.type("application/json").code(200);
-      return { id: codeDiff.id, iv: iv.toString("base64url") };
-    },
-  );
+    reply.type("application/json").code(200);
+    return { id: codeDiff.id, iv: iv.toString("base64url") };
+  });
 
   instance.post<{
     Reply: { html_url: string };
