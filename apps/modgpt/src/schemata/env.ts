a47c2a18f65ab17128f6a0ae84cbcda53a494a1e
Sasha Mysak <reactionnnnn@gmail.com>
2024-07-17 14:22:32 +0200

    add ai api url

--- apps/modgpt/src/schemata/env.ts
+++ apps/modgpt/src/schemata/env.ts
@@ -3,15 +3,21 @@ import {
   type Output,
   type ValiError,
   coerce,
+  literal,
   number,
   object,
   parse,
   string,
+  union,
 } from "valibot";
 
 dotenv.config();
 export const environmentSchema = object({
-  NODE_ENV: string(),
+  NODE_ENV: union([
+    literal("development"),
+    literal("production"),
+    literal("test"),
+  ]),
   PORT: coerce(number(), (input) => Number(input)),
   OPEN_AI_API_KEY: string(),
   CLAUDE_API_KEY: string(),
