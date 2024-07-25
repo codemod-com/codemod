f8bd4ed0a2e68b54e222cd646384f699a28a0e43
Sasha <sasha@codemod.com>
2024-07-25 15:09:02 +0200

    fix(cli): replace keytar with keyring-rs (#1148)

--- apps/cli/build.js
+++ apps/cli/build.js
@@ -45,7 +45,7 @@ build({
     "@ast-grep/napi",
     "@ast-grep/cli",
     "@octokit/rest",
-    "keytar",
+    "@napi-rs/keyring",
     "prettier",
     "blessed",
     // Workaround for @vue/compiler-sfc dynamic require
