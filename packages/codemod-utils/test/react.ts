24882e58562dd0e7ad2bf8d036cd9e7b3a7ed77d
Dmytro Hryshyn <dmytro@codemod.com>
2024-07-25 15:44:59 +0300

    react test

---
+++ packages/codemod-utils/test/react.ts
@@ -0,0 +1,59 @@
+import assert from "node:assert/strict";
+import jscodeshift, { type FileInfo, type API } from "jscodeshift";
+
+import { describe, it } from "vitest";
+import { getClassComponents } from "../src/index.js";
+
+const buildApi = (parser: string | undefined): API => ({
+  j: parser ? jscodeshift.withParser(parser) : jscodeshift,
+  jscodeshift: parser ? jscodeshift.withParser(parser) : jscodeshift,
+  stats: () => {
+    console.error(
+      "The stats function was called, which is not supported on purpose",
+    );
+  },
+  report: () => {
+    console.error(
+      "The report function was called, which is not supported on purpose",
+    );
+  },
+});
+
+const buildRootCollection = (file: FileInfo, api: API) => {
+  const j = api.jscodeshift;
+  const root = j(file.source);
+
+  return { j, root };
+};
+
+describe("react utils", async () => {
+  describe("getClassComponents", async () => {
+    it("Should support named import aliases", async () => {
+      const INPUT = `
+                import React1, { Component as Component1 } from 'react';
+        
+                class A extends React1.PureComponent {}
+                class B extends Component1 {}
+
+                class C extends React.Component {}
+                class D extends React1.Something {}
+                class F extends React1.Component {}
+            `;
+
+      const fileInfo: FileInfo = {
+        path: "index.js",
+        source: INPUT,
+      };
+
+      const { j, root } = buildRootCollection(fileInfo, buildApi("tsx"));
+
+      const components = getClassComponents(j, root);
+
+      assert.deepEqual(
+        components?.paths().map((p) => p.value.id.name),
+        ["A", "B", "F"],
+      );
+      assert.ok(components?.paths().length === 3);
+    });
+  });
+});
