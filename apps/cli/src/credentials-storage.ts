6f871c3e9e8519a934efed3d075f8efd4a62e7f7
Sasha Mysak <reactionnnnn@gmail.com>
2024-07-25 16:45:54 +0200

    remove existsSync

--- apps/cli/src/credentials-storage.ts
+++ apps/cli/src/credentials-storage.ts
@@ -1,7 +1,4 @@
-import * as os from "node:os";
-import keytar from "keytar";
-
-import { chalk } from "@codemod-com/printer";
+import { Entry } from "@napi-rs/keyring";
 
 export enum CredentialsStorageType {
   ACCOUNT = "user-account",
@@ -12,17 +9,14 @@ const SERVICE = `codemod.com${process.env.NODE_ENV === "production" ? "" : `-${p
 
 /**
  * A class to store and retrieve credentials securely.
- * It uses the `keytar` package to store the credentials.
+ * It uses the `keyring-rs` package to store the credentials.
  * The credentials are stored in the system's keychain.
- * On Linux, it uses `libsecret` to store the credentials.
- * On macOS, it uses the Keychain.
- * On Windows, it uses the Credential Vault.
  */
 export class CredentialsStorage {
   private _credentials: { [key in CredentialsStorageType]?: string } = {};
 
   async set(type: CredentialsStorageType, value: string) {
-    await keytar.setPassword(SERVICE, type, value);
+    new Entry(SERVICE, type).setPassword(value);
     this._credentials[type] = value;
   }
 
@@ -31,38 +25,15 @@ export class CredentialsStorage {
       return this._credentials[type];
     }
 
-    try {
-      const credentials = (await keytar.findCredentials(SERVICE)).find(
-        ({ account }) => account === type,
-      );
-      if (credentials) {
-        this._credentials[type] = credentials.password;
-        return credentials.password;
-      }
-    } catch (err) {
-      if (os.platform() === "linux") {
-        throw new Error(
-          chalk(
-            `Codemod CLI uses "keytar" to store your credentials securely.`,
-            `\nPlease make sure you have "libsecret" installed on your system.`,
-            "\nDepending on your distribution, you will need to run the following command",
-            "\nDebian/Ubuntu:",
-            chalk.bold("sudo apt-get install libsecret-1-dev"),
-            "\nFedora:",
-            chalk.bold("sudo dnf install libsecret"),
-            "\nArch Linux:",
-            chalk.bold("sudo pacman -S libsecret"),
-            `\n\n${String(err)}`,
-          ),
-        );
-      }
+    const password = new Entry(SERVICE, type).getPassword();
+    if (password === null) return null;
 
-      throw err;
-    }
+    this._credentials[type] = password;
+    return password;
   }
 
   async delete(type: CredentialsStorageType) {
-    await keytar.deletePassword(SERVICE, type);
+    new Entry(SERVICE, type).deletePassword();
     delete this._credentials[type];
   }
 }
