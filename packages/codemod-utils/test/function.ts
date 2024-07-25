a86ecf6bc91b071da1dab1d58cdc3a34ae0671d9
Dmytro Hryshyn <dmytro@codemod.com>
2024-07-25 18:46:52 +0300

    wip

--- packages/codemod-utils/test/function.ts
+++ packages/codemod-utils/test/function.ts
@@ -3,7 +3,7 @@ import jscodeshift, { type FileInfo, type API } from "jscodeshift";
 
 import { describe, it } from "vitest";
 
-import { isFunctionExportedByDefault } from "../src/index.js";
+import { getFunctionName, isFunctionExportedByDefault } from "../src/index.js";
 
 const buildApi = (parser: string | undefined): API => ({
   j: parser ? jscodeshift.withParser(parser) : jscodeshift,
@@ -28,48 +28,75 @@ const buildRootCollection = (file: FileInfo, api: API) => {
 };
 
 describe("function utils", async () => {
-  it("default export", async () => {
-    const INPUT = `
-    export default function A() {}
-    export function B() {}
-    `;
+  describe("isFunctionExportedByDefault", async () => {
+    it("default export", async () => {
+      const INPUT = `
+      export default function A() {}
+      export function B() {}
+      `;
 
-    const fileInfo: FileInfo = {
-      path: "index.js",
-      source: INPUT,
-    };
+      const fileInfo: FileInfo = {
+        path: "index.js",
+        source: INPUT,
+      };
 
-    const { j, root } = buildRootCollection(fileInfo, buildApi("tsx"));
+      const { j, root } = buildRootCollection(fileInfo, buildApi("tsx"));
 
-    const [fn1, fn2] = root.find(j.FunctionDeclaration).paths() ?? [];
+      const [fn1, fn2] = root.find(j.FunctionDeclaration).paths() ?? [];
 
-    assert.ok(fn1 !== undefined);
-    assert.ok(fn2 !== undefined);
+      assert.ok(fn1 !== undefined);
+      assert.ok(fn2 !== undefined);
 
-    assert.ok(isFunctionExportedByDefault(j, root, fn1));
-    assert.ok(!isFunctionExportedByDefault(j, root, fn2));
+      assert.ok(isFunctionExportedByDefault(j, root, fn1));
+      assert.ok(!isFunctionExportedByDefault(j, root, fn2));
+    });
+
+    it("default export 2", async () => {
+      const INPUT = `
+      const A = () => {}
+      const B = () => {}
+      export default A;
+      `;
+
+      const fileInfo: FileInfo = {
+        path: "index.js",
+        source: INPUT,
+      };
+
+      const { j, root } = buildRootCollection(fileInfo, buildApi("tsx"));
+
+      const [fn1, fn2] = root.find(j.ArrowFunctionExpression).paths() ?? [];
+
+      assert.ok(fn1 !== undefined);
+      assert.ok(fn2 !== undefined);
+
+      assert.ok(isFunctionExportedByDefault(j, root, fn1));
+      assert.ok(!isFunctionExportedByDefault(j, root, fn2));
+    });
   });
 
-  it("default export 2", async () => {
-    const INPUT = `
-    const A = () => {}
-    const B = () => {}
-    export default A;
-    `;
+  describe("getFunctionName", async () => {
+    it("it should correctly find functionName", async () => {
+      const INPUT = `
+        function A() {}
+        const B = () => {}
+      `;
 
-    const fileInfo: FileInfo = {
-      path: "index.js",
-      source: INPUT,
-    };
+      const fileInfo: FileInfo = {
+        path: "index.js",
+        source: INPUT,
+      };
 
-    const { j, root } = buildRootCollection(fileInfo, buildApi("tsx"));
+      const { j, root } = buildRootCollection(fileInfo, buildApi("tsx"));
 
-    const [fn1, fn2] = root.find(j.ArrowFunctionExpression).paths() ?? [];
+      const [fn1] = root.find(j.FunctionDeclaration).paths() ?? [];
+      const [fn2] = root.find(j.ArrowFunctionExpression).paths() ?? [];
 
-    assert.ok(fn1 !== undefined);
-    assert.ok(fn2 !== undefined);
+      assert.ok(fn1 !== undefined);
+      assert.ok(fn2 !== undefined);
 
-    assert.ok(isFunctionExportedByDefault(j, root, fn1));
-    assert.ok(!isFunctionExportedByDefault(j, root, fn2));
+      assert.ok(getFunctionName(j, fn1) === "A");
+      assert.ok(getFunctionName(j, fn2) === "B");
+    });
   });
 });
