2e82914e3bf2b5b8533c647dc5aaa656aa977e96
Sasha Mysak <reactionnnnn@gmail.com>
2024-07-27 17:47:35 +0200

    fix import

--- apps/backend/src/services/TelemetryService.ts
+++ apps/backend/src/services/TelemetryService.ts
@@ -1,7 +1,7 @@
 import { NullSender, PostHogSender } from "@codemod-com/telemetry";
 
-import { environment } from "src/util.js";
 import type { TelemetryEvents } from "../telemetry.js";
+import { environment } from "../util.js";
 
 export const telemetryService =
   environment.NODE_ENV === "production"
