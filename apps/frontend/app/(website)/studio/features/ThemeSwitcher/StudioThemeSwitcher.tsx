b6621d054483c6c93359d02463e5a4f8fe197a45
Greg <niebadz@gmail.com>
2024-07-29 20:29:21 +0200

    simplify for now

--- apps/frontend/app/(website)/studio/features/ThemeSwitcher/StudioThemeSwitcher.tsx
+++ apps/frontend/app/(website)/studio/features/ThemeSwitcher/StudioThemeSwitcher.tsx
@@ -1,16 +1,10 @@
-import { useTheme } from "@/hooks/useTheme";
 import { setMode } from "@features/ThemeSwitcher/setMode";
 import { Moon, Sun } from "@phosphor-icons/react";
 import { IconButton } from "@studio/components/button/IconButton";
 import { useEffect, useState } from "react";
 
 export const StudioThemeSwitcher = () => {
-  const globalTheme = useTheme();
-  const [isLight, setIsLight] = useState<boolean>();
-
-  useEffect(() => {
-    setIsLight(globalTheme.theme === "light");
-  }, [globalTheme.theme]);
+  const [isLight, setIsLight] = useState<boolean>(true);
 
   useEffect(() => {
     setMode(isLight);
