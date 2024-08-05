df0840601d3c32e88e6fb2aa88940c9ed0e2ebd8
Sasha <sasha@codemod.com>
2024-08-05 17:43:23 +0200

    feat(cli): staging build for CLI (#1185)

--- apps/cli/build.js
+++ apps/cli/build.js
@@ -1,6 +1,7 @@
 import { build } from "esbuild";
 import { hideBin } from "yargs/helpers";
 
+// Build envs for local production build, can be used by running `pnpm build --prod`
 const localProdBuildEnvs = {
   "process.env.NODE_ENV": '"production"',
   "process.env.BACKEND_URL": '"https://backend.codemod.com"',
@@ -10,11 +11,24 @@ const localProdBuildEnvs = {
   "process.env.IGNORE_TELEMETRY": "true",
 };
 
+// Build envs for staging, it is the default when running `pnpm build`
+const stagingBuildEnvs = {
+  "process.env.NODE_ENV": '"staging"',
+  "process.env.BACKEND_URL": '"https://staging-backend.codemod.com"',
+  "process.env.AUTH_BACKEND_URL": '"https://staging-backend.codemod.com/auth"',
+  "process.env.CODEMOD_HOME_PAGE_URL": '"https://staging.codemod.com"',
+  "process.env.CODEMOD_STUDIO_URL": '"https://staging.codemod.com/studio"',
+  "process.env.IGNORE_TELEMETRY": "true",
+};
+
+// Build envs for publishing to npm, it would usually happen during prepublishOnly script
 const publishEnvs = {
   ...localProdBuildEnvs,
   "process.env.IGNORE_TELEMETRY": "false",
 };
 
+// Build envs for local development where one would boot up the backend services and frontend locally
+// Can be used by running `pnpm build --local`
 const localEnvs = {
   "process.env.NODE_ENV": '"development"',
   "process.env.BACKEND_URL": '"http://localhost:8081"',
@@ -37,9 +51,11 @@ build({
   outfile: "./dist/index.cjs",
   define: argv.includes("--local")
     ? localEnvs
-    : argv.includes("--publish")
-      ? publishEnvs
-      : localProdBuildEnvs,
+    : argv.includes("--prod")
+      ? localProdBuildEnvs
+      : argv.includes("--publish")
+        ? publishEnvs
+        : stagingBuildEnvs,
   external: [
     "esbuild",
     "@ast-grep/napi",
