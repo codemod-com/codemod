ac6bf2851b66db55b9e97781619fc0cfc3b1b16e
Dmytro Hryshyn <dmytro@codemod.com>
2024-08-16 17:06:58 +0300

    clickbable table row

--- apps/frontend/app/(website)/campaigns/components/CampaignsTable.tsx
+++ apps/frontend/app/(website)/campaigns/components/CampaignsTable.tsx
@@ -4,11 +4,10 @@ import { Table } from "@radix-ui/themes";
 import Button from "@/components/shared/Button";
 import { useViewStore } from "@/store/view";
 import { MoreVertical, Star } from "lucide-react";
-import Link from "next/link";
+import { useRouter } from "next/navigation";
 import { memo, useMemo } from "react";
 import type { TabsConfig } from "../hooks/useCampaigTabsConfig";
 import type { Campaign } from "../hooks/useCampaigns";
-
 type InsightKind = TabsConfig[number]["kind"];
 
 const CampaignsTable = ({
@@ -24,6 +23,8 @@ const CampaignsTable = ({
     [campaignsSearchTerm, campaigns],
   );
 
+  const { push } = useRouter();
+
   return (
     <Table.Root>
       <Table.Header>
@@ -31,20 +32,19 @@ const CampaignsTable = ({
           <Table.ColumnHeaderCell>Name</Table.ColumnHeaderCell>
           <Table.ColumnHeaderCell>Updated On</Table.ColumnHeaderCell>
           <Table.ColumnHeaderCell>Owner</Table.ColumnHeaderCell>
-          <Table.ColumnHeaderCell>Link</Table.ColumnHeaderCell>
           <Table.ColumnHeaderCell> &nbsp;</Table.ColumnHeaderCell>
         </Table.Row>
       </Table.Header>
 
       <Table.Body>
         {filteredCampaigns.map((campaign) => (
-          <Table.Row>
+          <Table.Row
+            role="button"
+            onClick={() => push(`/campaigns/${campaign.id}`)}
+          >
             <Table.Cell>{campaign.name}</Table.Cell>
             <Table.Cell>{campaign.updatedAt}</Table.Cell>
             <Table.Cell>{campaign.owner}</Table.Cell>
-            <Table.Cell>
-              <Link href={`/campaigns/${campaign.id}`}>Open</Link>
-            </Table.Cell>
             <Table.Cell className="flex">
               <Button intent="secondary-icon-only" className="!border-0">
                 <Star size={16} />
