a2ef4b03d4d0198dc3fa0d2ae54aa3bc2f7c7b52
mirai2k <michael@codemod.com>
2024-08-23 17:58:59 +0300

    added staging handling & database config

--- apps/run-service/src/services/Redis.ts
+++ apps/run-service/src/services/Redis.ts
@@ -3,6 +3,11 @@ import { Queue } from "bullmq";
 import { Redis } from "ioredis";
 import { environment } from "../util.js";
 
+const TASK_MANAGER_QUEUE_NAME =
+  environment.NODE_ENV === "staging"
+    ? `${environment.TASK_MANAGER_QUEUE_NAME}_STAGING`
+    : environment.TASK_MANAGER_QUEUE_NAME;
+
 export const redis = environment.REDIS_HOST
   ? new Redis({
       host: String(environment.REDIS_HOST),
@@ -12,7 +17,7 @@ export const redis = environment.REDIS_HOST
   : null;
 
 export const queue = redis
-  ? new Queue<CodemodRunJobData>(environment.TASK_MANAGER_QUEUE_NAME ?? "", {
+  ? new Queue<CodemodRunJobData>(TASK_MANAGER_QUEUE_NAME ?? "", {
       connection: redis,
     })
   : null;
