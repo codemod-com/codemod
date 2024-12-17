079770c8524b2e3d5e873e7eeeba4bd43b343609
Siamak <siamak@users.noreply.github.com>
2024-12-17 23:15:26 +0330

    feat(react): add support for dark/light mode images and legacy image fallback (#1415)
    * feat(ui): Dark/light mode for Sanity image and support legacy images
    
    * chore: Support lightImage & darkImage type

--- apps/frontend/components/shared/pt.blocks/Image.tsx
+++ apps/frontend/components/shared/pt.blocks/Image.tsx
@@ -5,14 +5,12 @@ export default function ImageBlock(props: ImageBlock) {
   return (
     <div className="my-10 -mx-2 lg:-mx-10 flex flex-col gap-4">
       <div className="flex justify-center overflow-hidden rounded-xl">
-        {
-          <SanityImage
-            maxWidth={1440}
-            alt={props.image.alt || ""}
-            image={props.image}
-            elProps={{ className: "rounded-xl" }}
-          />
-        }
+        <SanityImage
+          maxWidth={1440}
+          alt={props.image.alt || ""}
+          image={props.image}
+          elProps={{ className: "rounded-xl" }}
+        />
       </div>
       {props.caption && (
         <p className="body-s-medium font-medium text-secondary-light dark:text-secondary-dark text-center pb-0">
