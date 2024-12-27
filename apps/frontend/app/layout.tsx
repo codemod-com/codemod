770e285d9b54fb9525900048efae857358eb6a96
Mohab Sameh <mohabsameh@outlook.com>
2024-12-27 20:17:03 +0200

    fix(website): gtag

--- apps/frontend/app/layout.tsx
+++ apps/frontend/app/layout.tsx
@@ -32,7 +32,10 @@ export default async function RootLayout({
           dangerouslySetInnerHTML={{ __html: mediaStyles }}
           type="text/css"
         />
-        <Script>
+        <Script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=AW-11521847611"
+        >
           {`
           function initApollo(){var n=Math.random().toString(36).substring(7),o=document.createElement("Script");
           o.src="https://assets.apollo.io/micro/website-tracker/tracker.iife.js?nocache="+n,o.async=!0,o.defer=!0,
@@ -40,12 +43,15 @@ export default async function RootLayout({
           document.head.appendChild(o)}
         initApollo();`}
         </Script>
+        <Script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=AW-11521847611"
+        >
+          {`
+          `}
+        </Script>
         <Script>
           {`
-          <!-- Google tag (gtag.js) -->
-          <script async src="https://www.googletagmanager.com/gtag/js?id=AW-11521847611">
-          </script>
-          <script>
             window.dataLayer = window.dataLayer || [];
             function gtag(){dataLayer.push(arguments);}
             gtag('js', new Date());
