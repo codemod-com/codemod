801b13fe93fbd5118f4d6f71ab05329db37deba6
Mohab Sameh <mohabsameh@outlook.com>
2025-01-28 19:38:25 +0200

    chore(frontend): add gtag manager snippet

--- apps/frontend/app/layout.tsx
+++ apps/frontend/app/layout.tsx
@@ -25,6 +25,13 @@ export default async function RootLayout({
 						n&&j.setAttribute('nonce',n.nonce||n.getAttribute('nonce'));f.parentNode.insertBefore(j,f);
 						})(window,document,'script','dataLayer','GTM-K32HQ25J');`}
         </Script>
+        <Script id="gtm">
+          {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+          })(window,document,'script','dataLayer','GTM-PBSBXXDS');`}
+        </Script>
         <script dangerouslySetInnerHTML={{ __html: dynamicFavicon }} />
         <script dangerouslySetInnerHTML={{ __html: themeScript }} />
         <style
@@ -60,6 +67,16 @@ export default async function RootLayout({
         </Script>
       </head>
       <body>
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-PBSBXXDS"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          />
+        </noscript>
+        {/* End Google Tag Manager (noscript) */}
         {children}
         <Analytics />
       </body>
