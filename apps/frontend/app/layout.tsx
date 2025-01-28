81023670864144322bcc7dab22b3e0fb69c3d93e
Mohab Sameh <37941642+mohab-sameh@users.noreply.github.com>
2025-01-28 20:33:25 +0200

    fix gtag (#1468)

--- apps/frontend/app/layout.tsx
+++ apps/frontend/app/layout.tsx
@@ -25,7 +25,7 @@ export default async function RootLayout({
 						n&&j.setAttribute('nonce',n.nonce||n.getAttribute('nonce'));f.parentNode.insertBefore(j,f);
 						})(window,document,'script','dataLayer','GTM-K32HQ25J');`}
         </Script>
-        <Script id="gtm">
+        <Script>
           {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
           new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
           j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
