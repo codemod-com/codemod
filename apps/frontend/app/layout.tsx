512b561345402aca8a0c9c083641b1e50bcf8351
Mohab Sameh <mohabsameh@outlook.com>
2025-01-31 13:03:47 +0200

    fix(website): move ga4 tag higher

--- apps/frontend/app/layout.tsx
+++ apps/frontend/app/layout.tsx
@@ -17,6 +17,13 @@ export default async function RootLayout({
   return (
     <html lang="en" className={cx(globalFontsVariables, "scroll-smooth light")}>
       <head>
+        <Script>
+          {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+          })(window,document,'script','dataLayer','GTM-PBSBXXDS');`}
+        </Script>
         <Script id="gtm">
           {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
 						new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
@@ -24,13 +31,6 @@ export default async function RootLayout({
 						'https://www.googletagmanager.com/gtm.js?id='+i+dl;var n=d.querySelector('[nonce]');
 						n&&j.setAttribute('nonce',n.nonce||n.getAttribute('nonce'));f.parentNode.insertBefore(j,f);
 						})(window,document,'script','dataLayer','GTM-K32HQ25J');`}
-        </Script>
-        <Script>
-          {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-          })(window,document,'script','dataLayer','GTM-PBSBXXDS');`}
         </Script>
         <script dangerouslySetInnerHTML={{ __html: dynamicFavicon }} />
         <script dangerouslySetInnerHTML={{ __html: themeScript }} />
