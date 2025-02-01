cc1f701986997965b4c902b1041eb63fa79d1a99
Siamak <siamak@users.noreply.github.com>
2025-02-01 12:14:08 +0330

    Update layout.tsx

--- apps/frontend/app/layout.tsx
+++ apps/frontend/app/layout.tsx
@@ -2,8 +2,7 @@ import { mediaStyles } from "@/components/global/Media";
 import globalFontsVariables from "@/fonts";
 import dynamicFavicon from "@/headScripts/dynamic_favicon";
 import themeScript from "@/headScripts/theme";
-import { GoogleTagManager } from "@next/third-parties/google";
-import { GoogleAnalytics } from "@next/third-parties/google";
+import { GoogleTagManager, GoogleAnalytics } from "@next/third-parties/google";
 import { Analytics } from "@vercel/analytics/react";
 import { cx } from "cva";
 
