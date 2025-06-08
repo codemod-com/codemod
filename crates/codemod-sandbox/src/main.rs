39f6dbcd677e0d4474851e077be8e9cf7bee2df4
Mohamad Mohebifar <mo@codemod.com>
2025-06-08 01:14:57 -0700

    fix: Fix WASI lint errors

--- crates/codemod-sandbox/src/main.rs
+++ crates/codemod-sandbox/src/main.rs
@@ -1,11 +1,16 @@
-mod ast_grep;
-mod capabilities;
-mod plugins;
-mod rquickjs_compat;
-mod sandbox;
-mod utils;
+#[cfg(feature = "wasm")]
+mod wasi_bin {
+    mod ast_grep;
+    mod capabilities;
+    mod plugins;
+    mod rquickjs_compat;
+    mod sandbox;
+    mod utils;
+
+    pub use crate::sandbox::wasm::*;
+}
 
 #[cfg(feature = "wasm")]
-pub use crate::sandbox::wasm::*;
+pub use wasi_bin::*;
 
 pub fn main() {}
