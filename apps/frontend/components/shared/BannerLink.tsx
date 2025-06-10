8dee6bc70434d8aadaeadf84d51fbeb27894ff63
Siamak <siamak@users.noreply.github.com>
2025-06-10 21:13:09 +0330

    fix(website): update navigation item (#1534)
    * fix(website): update navigation item
    
    * fix(linter) fix indents
    
    * fix(lint): Fixed linter error

--- apps/frontend/components/shared/BannerLink.tsx
+++ apps/frontend/components/shared/BannerLink.tsx
@@ -1,6 +1,5 @@
 "use client";
-import { cn } from "@/utils";
-import { ChevronRight, Languages } from "lucide-react";
+import { ChevronRight } from "lucide-react";
 import { usePathname } from "next/navigation";
 import { AnimatedGradientLink } from "./AnimatedGradientText";
 
@@ -10,14 +9,12 @@ export const BannerLink = () => {
 
   if (!isHomePage) return null;
   return (
-    <AnimatedGradientLink href="/i18n" className="inline-flex gap-2">
-      <Languages className="size-4" />
-      <span
-        className={cn(
-          `animate-gradient inline bg-gradient-to-r dark:from-accent dark:via-info-light dark:to-accent from-success-dark via-info-dark font-bold dark:font-medium to-success-dark bg-[length:var(--bg-size)_100%] bg-clip-text text-transparent`,
-        )}
-      >
-        New: i18n codemods
+    <AnimatedGradientLink
+      href="https://app.codemod.com/pages/insights"
+      className="inline-flex gap-2"
+    >
+      <span className="animate-gradient inline bg-gradient-to-r dark:from-accent dark:via-info-light dark:to-accent from-success-dark via-info-dark font-bold dark:font-medium to-success-dark bg-[length:var(--bg-size)_100%] bg-clip-text text-transparent">
+        New: Codemod Insights for engineering leaders
       </span>
       <ChevronRight className="size-4 transition-transform duration-300 ease-in-out group-hover:translate-x-0.5" />
     </AnimatedGradientLink>
