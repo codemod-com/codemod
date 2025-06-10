d2498dade2e1273de26ad7adc2c2d5e1d7e8afb5
Siamak <s.mokhtari75@gmail.com>
2025-06-10 21:11:24 +0330

    fix(lint): Fixed linter error

--- apps/frontend/components/shared/BannerLink.tsx
+++ apps/frontend/components/shared/BannerLink.tsx
@@ -1,5 +1,4 @@
 "use client";
-import { cn } from "@/utils";
 import { ChevronRight } from "lucide-react";
 import { usePathname } from "next/navigation";
 import { AnimatedGradientLink } from "./AnimatedGradientText";
@@ -10,12 +9,11 @@ export const BannerLink = () => {
 
   if (!isHomePage) return null;
   return (
-    <AnimatedGradientLink href="https://app.codemod.com/pages/insights" className="inline-flex gap-2">
-      <span
-        className={cn(
-          `animate-gradient inline bg-gradient-to-r dark:from-accent dark:via-info-light dark:to-accent from-success-dark via-info-dark font-bold dark:font-medium to-success-dark bg-[length:var(--bg-size)_100%] bg-clip-text text-transparent`,
-        )}
-      >
+    <AnimatedGradientLink
+      href="https://app.codemod.com/pages/insights"
+      className="inline-flex gap-2"
+    >
+      <span className="animate-gradient inline bg-gradient-to-r dark:from-accent dark:via-info-light dark:to-accent from-success-dark via-info-dark font-bold dark:font-medium to-success-dark bg-[length:var(--bg-size)_100%] bg-clip-text text-transparent">
         New: Codemod Insights for engineering leaders
       </span>
       <ChevronRight className="size-4 transition-transform duration-300 ease-in-out group-hover:translate-x-0.5" />
