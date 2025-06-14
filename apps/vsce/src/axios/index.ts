330f886f4f0144ceda85f943a19915d1f4ea2218
Mohamad Mohebifar <mo@codemod.com>
2025-06-14 09:19:46 -0700

    chore(pr): Remove unused packages (ai, run-service, and vsce)

--- apps/vsce/src/axios/index.ts
+++
@@ -1,8 +0,0 @@
-import axios from "axios";
-import axiosRetry from "axios-retry";
-
-const retryingClient = axios.create();
-axiosRetry(retryingClient);
-
-export const DEFAULT_RETRY_COUNT = 3;
-export { retryingClient };
--- apps/vsce/src/telemetry/hashes.ts
+++
@@ -1,5 +0,0 @@
-import { randomBytes } from "node:crypto";
-import type { CaseHash } from "../cases/types";
-
-export const buildCaseHash = (): CaseHash =>
-  randomBytes(20).toString("base64url") as CaseHash;
