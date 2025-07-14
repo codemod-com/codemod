9b9e3fe179bc567f1e504575cf002c6a731295fa
Amirabbas Ghasemi <amirabbas08dev@gmail.com>
2025-07-14 11:19:51 +0330

    feat: create telemetry crate

---
+++ crates/telemetry/src/send_event.rs
@@ -0,0 +1,72 @@
+use std::collections::HashMap;
+use posthog_rs;
+use serde::Serialize;
+use async_trait::async_trait;
+
+#[derive(Debug, Clone)]
+pub struct TelemetrySenderOptions {
+    pub distinct_id: String,
+    pub cloud_role: String,
+}
+
+#[derive(Debug, Clone)]
+pub struct PartialTelemetrySenderOptions {
+    pub distinct_id: Option<String>,
+    pub cloud_role: Option<String>,
+}
+
+#[derive(Debug, Serialize, Clone)]
+pub struct BaseEvent {
+    pub kind: String,
+    #[serde(flatten)]
+    pub properties: HashMap<String, String>,
+}
+
+#[async_trait]
+pub trait TelemetrySender {
+    async fn send_event(
+        &self,
+        event: BaseEvent,
+        options_override: Option<PartialTelemetrySenderOptions>,
+    );
+}
+
+pub struct PostHogSender {
+    client: posthog_rs::Client,
+    options: TelemetrySenderOptions,
+}
+
+impl PostHogSender {
+    pub async fn new(options: TelemetrySenderOptions) -> Self {
+        let client = posthog_rs::client("phc_nGWKWP3t1fcNFqGi6UdstXjMf0fxx7SBeohHPSS6d2Y").await;
+        Self { client, options }
+    }
+}
+
+#[async_trait]
+impl TelemetrySender for PostHogSender {
+    async fn send_event(
+        &self,
+        event: BaseEvent,
+        options_override: Option<PartialTelemetrySenderOptions>,
+    ) {
+        let distinct_id = options_override
+            .as_ref()
+            .and_then(|o| o.distinct_id.clone())
+            .unwrap_or_else(|| self.options.distinct_id.clone());
+
+        let cloud_role = options_override
+            .as_ref()
+            .and_then(|o| o.cloud_role.clone())
+            .unwrap_or_else(|| self.options.cloud_role.clone());
+
+        let posthog_event = posthog_rs::Event::new(
+            format!("codemod.{}.{}", cloud_role, event.kind),
+            distinct_id.clone()
+        );
+
+        if let Err(e) = self.client.capture(posthog_event).await {
+            eprintln!("Failed to send PostHog event: {e}");
+        }
+    }
+}
